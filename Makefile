b659cffe4c7a30533a3920ca7cbeac17918657b1
MarcoFavorito <marco.favorito@gmail.com>
2021-07-14 11:45:46 +0200

    feat: set up project

---
+++ Makefile
@@ -0,0 +1,123 @@
+.PHONY: clean
+clean: clean-build clean-pyc clean-test clean-docs
+
+.PHONY: clean-build
+clean-build:
+	rm -fr build/
+	rm -fr dist/
+	rm -fr .eggs/
+	rm -fr pip-wheel-metadata
+	find . -name '*.egg-info' -exec rm -fr {} +
+	find . -name '*.egg' -exec rm -fr {} +
+	rm -fr Pipfile.lock
+
+.PHONY: clean-docs
+clean-docs:
+	rm -fr site/
+
+.PHONY: clean-pyc
+clean-pyc:
+	find . -name '*.pyc' -exec rm -f {} +
+	find . -name '*.pyo' -exec rm -f {} +
+	find . -name '*~' -exec rm -f {} +
+	find . -name '__pycache__' -exec rm -fr {} +
+	find . -name '.DS_Store' -exec rm -fr {} +
+
+.PHONY: clean-test
+clean-test:
+	rm -fr .tox/
+	rm -f .coverage
+	find . -name ".coverage*" -not -name ".coveragerc" -exec rm -fr "{}" \;
+	rm -fr coverage.xml
+	rm -fr htmlcov/
+	rm -fr .hypothesis
+	rm -fr .pytest_cache
+	rm -fr .mypy_cache/
+	find . -name 'log.txt' -exec rm -fr {} +
+	find . -name 'log.*.txt' -exec rm -fr {} +
+
+.PHONY: lint
+lint:
+	black aea_consensus_algorithms packages scripts tests
+	isort aea_consensus_algorithms packages scripts tests
+	flake8 aea_consensus_algorithms packages scripts tests
+	vulture aea_consensus_algorithms scripts/whitelist.py
+	darglint aea_consensus_algorithms packages scripts
+
+.PHONY: pylint
+pylint:
+	pylint -j4 aea_consensus_algorithms packages scripts
+
+.PHONY: security
+security:
+	bandit -r aea_consensus_algorithms packages
+	bandit -s B101 -r tests scripts
+	safety check -i 37524 -i 38038 -i 37776 -i 38039 -i 39621 -i 40291 -i 39706
+
+.PHONY: static
+static:
+	mypy aea_consensus_algorithms packages scripts --disallow-untyped-defs
+	mypy tests
+
+.PHONY: package_checks
+package_checks:
+	python scripts/generate_ipfs_hashes.py --check
+	python scripts/check_package_versions_in_docs.py
+	python scripts/check_packages.py
+
+.PHONY: docs
+docs:
+	mkdocs build --clean
+
+.PHONY: common_checks
+common_checks: security misc_checks lint static docs
+
+.PHONY: test
+test:
+	pytest -rfE --doctest-modules aea_consensus_algorithms packages tests/ --cov=aea_consensus_algorithms --cov-report=html --cov-report=xml --cov-report=term-missing --cov-report=term --cov-config=.coveragerc
+	find . -name ".coverage*" -not -name ".coveragerc" -exec rm -fr "{}" \;
+
+.PHONY: test-all
+test-all:
+	tox
+
+.PHONY: install
+install: clean
+	python3 setup.py install
+
+.PHONY: dist
+dist: clean
+	python setup.py sdist
+	WIN_BUILD_WHEEL=1 python setup.py bdist_wheel --plat-name=win_amd64
+	WIN_BUILD_WHEEL=1 python setup.py bdist_wheel --plat-name=win32
+	python setup.py bdist_wheel --plat-name=manylinux1_x86_64
+	python setup.py bdist_wheel --plat-name=manylinux2014_aarch64
+	python setup.py bdist_wheel --plat-name=macosx_10_9_x86_64
+
+h := $(shell git rev-parse --abbrev-ref HEAD)
+
+.PHONY: release_check
+release:
+	if [ "$h" = "main" ];\
+	then\
+		echo "Please ensure everything is merged into main & tagged there";\
+		pip install twine;\
+		twine upload dist/*;\
+	else\
+		echo "Please change to main branch for release.";\
+	fi
+
+v := $(shell pip -V | grep virtualenvs)
+
+.PHONY: new_env
+new_env: clean
+	if [ -z "$v" ];\
+	then\
+		pipenv --rm;\
+		pipenv --python 3.7;\
+		pipenv install --dev --skip-lock --clear;\
+		pipenv run pip install -e .[all];\
+		echo "Enter virtual environment with all development dependencies now: 'pipenv shell'.";\
+	else\
+		echo "In a virtual environment! Exit first: 'exit'.";\
+	fi
---
+++ scripts/check_copyright.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2021 Valory Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""
+This script checks that all the Python files of the repository have the copyright notice.
+
+In particular:
+- (optional) the Python shebang
+- the encoding header;
+- the copyright and license notices;
+
+It is assumed the script is run from the repository root.
+"""
+
+import argparse
+import itertools
+import re
+import sys
+from pathlib import Path
+
+
+HEADER_REGEX = re.compile(
+    r"""(#!/usr/bin/env python3
+)?# -\*- coding: utf-8 -\*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2021 Valory Limited
+#
+#   Licensed under the Apache License, Version 2\.0 \(the \"License\"\);
+#   you may not use this file except in compliance with the License\.
+#   You may obtain a copy of the License at
+#
+#       http://www\.apache\.org/licenses/LICENSE-2\.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an \"AS IS\" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied\.
+#   See the License for the specific language governing permissions and
+#   limitations under the License\.
+#
+# ------------------------------------------------------------------------------
+""",
+    re.MULTILINE,
+)
+
+
+def check_copyright(file: Path) -> bool:
+    """
+    Given a file, check if the header stuff is in place.
+
+    Return True if the files has the encoding header and the copyright notice,
+    optionally prefixed by the shebang. Return False otherwise.
+
+    :param file: the file to check.
+    :return: True if the file is compliant with the checks, False otherwise.
+    """
+    content = file.read_text()
+    return re.match(HEADER_REGEX, content) is not None
+
+
+def parse_args():
+    """Parse arguments."""
+    parser = argparse.ArgumentParser("check_copyright_notice")
+    parser.add_argument(
+        "--directory", type=str, default=".", help="The path to the repository root."
+    )
+
+
+if __name__ == "__main__":
+    exclude_files = {Path("scripts", "whitelist.py")}
+    python_files = filter(
+        lambda x: x not in exclude_files,
+        itertools.chain(
+            Path("aea_consensus_algorithm").glob("**/*.py"),
+            Path("packages").glob("**/*.py"),
+            Path("tests").glob("**/*.py"),
+            Path("scripts").glob("**/*.py"),
+        ),
+    )
+
+    bad_files = set()
+    for path in python_files:
+        print("Processing {}".format(path))
+        if not check_copyright(path):
+            bad_files.add(path)
+
+    if len(bad_files) > 0:
+        print("The following files are not well formatted:")
+        print("\n".join(map(str, bad_files)))
+        sys.exit(1)
+    else:
+        print("OK")
+        sys.exit(0)
