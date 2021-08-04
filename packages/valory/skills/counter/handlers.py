0b8aa0fee6b3626ef09b7b8b2b14a6afbc330630
MarcoFavorito <marco.favorito@gmail.com>
2021-08-04 08:57:34 +0200

    feat: add counter abci skill
    the counter skill implements the ABCI counter app, a
    toy example described in the Tendermint documentaiton.

---
+++ packages/valory/skills/counter/handlers.py
@@ -0,0 +1,182 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2018-2019 Fetch.AI Limited
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This module contains the handler for the 'abci' skill."""
+import struct
+from typing import Any, cast
+
+from packages.valory.protocols.abci import AbciMessage
+from packages.valory.protocols.abci.custom_types import Events, ProofOps
+from packages.valory.skills.abstract_abci.handlers import ABCIHandler
+from packages.valory.skills.counter.dialogues import AbciDialogue
+
+
+OK_CODE = 0
+ERROR_CODE = 1
+
+
+def encode_number(value: int) -> bytes:
+    """Encode an integer (big-endian)."""
+    return struct.pack(">I", value)
+
+
+def decode_number(raw: bytes) -> int:
+    """Decode an integer (little-endian)."""
+    return int.from_bytes(raw, byteorder="big")
+
+
+class ABCICounterHandler(ABCIHandler):
+    """ABCI counter handler."""
+
+    SUPPORTED_PROTOCOL = AbciMessage.protocol_id
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Initialize the handler."""
+        super().__init__(*args, **kwargs)
+        self.tx_count = 0
+        self.last_block_height = 0
+
+    def info(self, message: AbciMessage, dialogue: AbciDialogue) -> AbciMessage:
+        """
+        Handle a message of REQUEST_INFO performative.
+
+        :param message: the ABCI request.
+        :param dialogue: the ABCI dialogue.
+        :return: the response.
+        """
+        info_data = ""
+        version = message.version
+        app_version = 0
+        last_block_height = 0
+        last_block_app_hash = b""
+        reply = dialogue.reply(
+            performative=AbciMessage.Performative.RESPONSE_INFO,
+            target_message=message,
+            info_data=info_data,
+            version=version,
+            app_version=app_version,
+            last_block_height=last_block_height,
+            last_block_app_hash=last_block_app_hash,
+        )
+        return cast(AbciMessage, reply)
+
+    def init_chain(self, message: AbciMessage, dialogue: AbciDialogue) -> AbciMessage:
+        """
+        Handle a message of REQUEST_INIT_CHAIN performative.
+
+        :param message: the ABCI request.
+        :param dialogue: the ABCI dialogue.
+        :return: the response.
+        """
+        self.tx_count = 0
+        self.last_block_height = 0
+        return super().init_chain(message, dialogue)
+
+    def query(self, message: AbciMessage, dialogue: AbciDialogue) -> AbciMessage:
+        """
+        Handle a message of REQUEST_QUERY performative.
+
+        :param message: the ABCI request.
+        :param dialogue: the ABCI dialogue.
+        :return: the response.
+        """
+        v = encode_number(self.tx_count)
+        log = f"value: {self.tx_count}"
+        reply = dialogue.reply(
+            performative=AbciMessage.Performative.RESPONSE_QUERY,
+            code=OK_CODE,
+            log=log,
+            info="",
+            index=0,
+            key=b"",
+            value=v,
+            proof_ops=ProofOps([]),
+            height=self.last_block_height,
+            codespace="",
+        )
+        return cast(AbciMessage, reply)
+
+    def check_tx(self, message: AbciMessage, dialogue: AbciDialogue) -> AbciMessage:
+        """
+        Handle a message of REQUEST_CHECK_TX performative.
+
+        :param message: the ABCI request.
+        :param dialogue: the ABCI dialogue.
+        :return: the response.
+        """
+        value = decode_number(message.tx)
+        if value == (self.tx_count + 1):
+            code = OK_CODE
+            log = "valid transaction."
+            info = "OK: the next count is a unitary increment."
+        else:
+            code = ERROR_CODE
+            log = "invalid transaction."
+            info = "ERROR: the next count must be a unitary increment."
+
+        reply = dialogue.reply(
+            performative=AbciMessage.Performative.RESPONSE_CHECK_TX,
+            code=code,
+            data=b"",
+            log=log,
+            info=info,
+            gas_wanted=0,
+            gas_used=0,
+            events=Events([]),
+            codespace="",
+        )
+        return cast(AbciMessage, reply)
+
+    def deliver_tx(self, message: AbciMessage, dialogue: AbciDialogue) -> AbciMessage:
+        """
+        Handle a message of REQUEST_DELIVER_TX performative.
+
+        :param message: the ABCI request.
+        :param dialogue: the ABCI dialogue.
+        :return: the response.
+        """
+        self.tx_count += 1
+        reply = dialogue.reply(
+            performative=AbciMessage.Performative.RESPONSE_DELIVER_TX,
+            code=OK_CODE,
+            data=b"",
+            log="",
+            info="",
+            gas_wanted=0,
+            gas_used=0,
+            events=Events([]),
+            codespace="",
+        )
+        return cast(AbciMessage, reply)
+
+    def commit(self, message: AbciMessage, dialogue: AbciDialogue) -> AbciMessage:
+        """
+        Handle a message of REQUEST_COMMIT performative.
+
+        :param message: the ABCI request.
+        :param dialogue: the ABCI dialogue.
+        :return: the response.
+        """
+        hash_ = struct.pack(">Q", self.tx_count)
+        reply = dialogue.reply(
+            performative=AbciMessage.Performative.RESPONSE_COMMIT,
+            data=hash_,
+            retain_height=0,
+        )
+        return cast(AbciMessage, reply)
---
+++ tests/test_skills_integration/test_counter.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2021 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""Integration tests for the valory/counter skill."""
+import time
+
+from aea.test_tools.test_cases import AEATestCaseEmpty
+
+from tests.conftest import UseTendermint
+
+
+class TestABCICounterSkill(AEATestCaseEmpty, UseTendermint):
+    """Test that the ABCI counter skill works together with Tendermint."""
+
+    IS_LOCAL = False
+    capture_log = True
+
+    def test_run(self):
+        """Run the ABCI skill."""
+        self.generate_private_key()
+        self.add_private_key()
+        self.add_item("skill", "valory/counter:0.1.0")
+        self.set_config(
+            "vendor.valory.connections.abci.config.target_skill_id",
+            "valory/counter:0.1.0",
+        )
+
+        process = self.run_agent()
+        is_running = self.is_running(process)
+        assert is_running, "AEA not running within timeout!"
+
+        time.sleep(2.0)
+
+        check_strings = (
+            "ABCI Handler: setup method called.",
+            "Received ABCI request of type info",
+            "Received ABCI request of type flush",
+            "Received ABCI request of type begin_block",
+            "Received ABCI request of type commit",
+            "Received ABCI request of type end_block",
+        )
+        missing_strings = self.missing_from_output(process, check_strings)
+        assert (
+            missing_strings == []
+        ), "Strings {} didn't appear in agent output.".format(missing_strings)
+
+        assert (
+            self.is_successfully_terminated()
+        ), "ABCI agent wasn't successfully terminated."
