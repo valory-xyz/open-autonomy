2adfc61e7f7c28b160c93e704c72be7273c0e313
dvilela <dvilelaf@gmail.com>
2021-09-21 18:35:44 +0200

    feat: let keeper timeout be a configurable value

--- examples/price_estimation/configure_agents/abci3.sh
+++ examples/price_estimation/configure_agents/abci3.sh
@@ -3,6 +3,7 @@
 cp ../configure_agents/keys/ethereum_private_key_3.txt ethereum_private_key.txt
 aea config set vendor.valory.skills.price_estimation_abci.models.price_api.args.source_id binance
 aea config set vendor.valory.skills.price_estimation_abci.models.params.args.consensus.max_participants 4
+aea config set vendor.valory.skills.price_estimation_abci.models.params.args.consensus.keeper_timeout_seconds 5
 aea config set vendor.valory.skills.price_estimation_abci.models.params.args.tendermint_url http://node3:26657
 aea config set vendor.valory.skills.price_estimation_abci.models.params.args.convert_id USDT
 aea config set vendor.valory.skills.price_estimation_abci.models.params.args.ethereum_node_url http://hardhat:8545
