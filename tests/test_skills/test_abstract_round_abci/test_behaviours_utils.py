7ec232e74991a4201a5599291629514fc5c28a73
MarcoFavorito <marco.favorito@gmail.com>
2021-09-26 15:44:17 +0200

    test: improve coverage of BaseState class

--- tests/test_skills/test_abstract_round_abci/test_behaviours_utils.py
+++ tests/test_skills/test_abstract_round_abci/test_behaviours_utils.py
@@ -21,12 +21,21 @@
 import time
 from abc import ABC
 from datetime import datetime
-from typing import Generator
+from typing import Generator, Optional, Tuple
+from unittest import mock
+from unittest.mock import MagicMock
 
 import pytest
 
+from packages.valory.skills.abstract_round_abci.base import (
+    AbstractRound,
+    BasePeriodState,
+)
 from packages.valory.skills.abstract_round_abci.behaviour_utils import (
     AsyncBehaviour,
+    BaseState,
+    DONE_EVENT,
+    FAIL_EVENT,
     SendException,
 )
 
@@ -115,7 +124,7 @@ def test_async_behaviour_wait_for_message():
         behaviour.try_send("wrong_message")
     behaviour.act()
 
-    # sending the right message wiill transition to the next state,
+    # sending the right message will transition to the next state,
     # but only when calling act()
     behaviour.try_send(expected_message)
     assert behaviour.counter == 1
@@ -232,3 +241,78 @@ def test_async_behaviour_raise_stopiteration():
     behaviour = MyAsyncBehaviour()
     behaviour.act()
     assert behaviour.state == AsyncBehaviour.AsyncState.READY
+
+
+class RoundA(AbstractRound):
+    """Concrete ABCI round."""
+
+    def end_block(self) -> Optional[Tuple[BasePeriodState, "AbstractRound"]]:
+        """Handle end block."""
+        return None
+
+    round_id = "round_a"
+
+
+class StateATest(BaseState):
+    """Concrete BaseState class."""
+
+    state_id = "state_a"
+    matching_round = RoundA
+
+    def async_act(self) -> Generator:
+        """Do the 'async_act'."""
+
+
+class TestBaseState:
+    """Tests for the 'BaseState' class."""
+
+    def setup(self):
+        """Set up the tests."""
+        self.context_mock = MagicMock()
+        self.behaviour = StateATest(name="", skill_context=self.context_mock)
+
+    def test_check_in_round(self):
+        """Test 'BaseState' initialization."""
+        expected_round_id = "round"
+        self.context_mock.state.period.current_round_id = expected_round_id
+        assert self.behaviour.check_in_round(expected_round_id)
+        assert not self.behaviour.check_in_round("wrong round")
+
+        assert not self.behaviour.check_not_in_round(expected_round_id)
+        assert self.behaviour.check_not_in_round("wrong round")
+
+        func = self.behaviour.is_round_ended(expected_round_id)
+        assert not func()
+
+    def test_wait_until_round_end_negative_no_matching_round(self):
+        """Test 'wait_until_round_end' method, negative case (no matching round)."""
+        self.behaviour.matching_round = None
+        generator = self.behaviour.wait_until_round_end()
+        with pytest.raises(ValueError):
+            generator.send(None)
+
+    def test_set_done(self):
+        """Test 'set_done' method."""
+        assert not self.behaviour.is_done()
+        self.behaviour.set_done()
+        assert self.behaviour.is_done()
+        assert self.behaviour._event == DONE_EVENT
+
+    def test_set_fail(self):
+        """Test 'set_fail' method."""
+        assert not self.behaviour.is_done()
+        self.behaviour.set_fail()
+        assert self.behaviour.is_done()
+        assert self.behaviour._event == FAIL_EVENT
+
+    def test_send_a2a_transaction_negative_no_matching_round(self):
+        """Test 'send_a2a_transaction' method, negative case (no matching round)."""
+        self.behaviour.matching_round = None
+        generator = self.behaviour.send_a2a_transaction(MagicMock())
+        with pytest.raises(ValueError, match="No matching_round set!"):
+            generator.send(None)
+
+    @mock.patch.object(BaseState, "_send_transaction")
+    def test_send_a2a_transaction_positive(self, *_):
+        """Test 'send_a2a_transaction' method, positive case."""
+        self.behaviour.send_a2a_transaction(MagicMock())
