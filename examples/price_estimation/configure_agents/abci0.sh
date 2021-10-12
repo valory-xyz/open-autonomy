e60dd45d13d6dde312bfdf30622a456a3ea2d4db
David Minarsch <david.minarsch@googlemail.com>
2021-10-12 10:22:32 +0100

    fix: update examples config

--- examples/price_estimation/configure_agents/abci0.sh
+++ examples/price_estimation/configure_agents/abci0.sh
@@ -3,8 +3,7 @@
 cp ../configure_agents/keys/ethereum_private_key_0.txt ethereum_private_key.txt
 aea config set vendor.valory.skills.price_estimation_abci.models.price_api.args.source_id coingecko
 aea config set vendor.valory.skills.price_estimation_abci.models.params.args.consensus.max_participants 4
-aea config set vendor.valory.skills.price_estimation_abci.models.params.args.consensus.keeper_timeout_seconds 5
+aea config set vendor.valory.skills.price_estimation_abci.models.params.args.keeper_timeout_seconds 5
 aea config set vendor.valory.skills.price_estimation_abci.models.params.args.tendermint_url http://node0:26657
-aea config set vendor.valory.skills.price_estimation_abci.models.params.args.ethereum_node_url http://hardhat:8545
 aea config set vendor.fetchai.connections.ledger.config.ledger_apis.ethereum.address http://hardhat:8545
 aea build
