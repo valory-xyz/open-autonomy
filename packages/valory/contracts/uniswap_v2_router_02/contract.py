075a013449715633acffa570bd9cf7b258c62d39
dvilela <dvilelaf@gmail.com>
2021-10-19 18:25:20 +0200

    Fixing license

--- packages/valory/contracts/uniswap_v2_router_02/contract.py
+++ packages/valory/contracts/uniswap_v2_router_02/contract.py
@@ -3,9 +3,7 @@
 #
 #   Copyright 2021 Valory AG
 #
-#   Licensed under the Apache License, Version 2.0 (
-# cls,the "License"):
-#   pass;
+#   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
 #   You may obtain a copy of the License at
 #
