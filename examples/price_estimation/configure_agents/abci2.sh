4906fa4a1e7c695e23089cd822fef88186d85d76
David Minarsch <david.minarsch@googlemail.com>
2021-11-09 10:13:26 +0000

    chore: add relevant observation range

--- examples/price_estimation/configure_agents/abci2.sh
+++ examples/price_estimation/configure_agents/abci2.sh
@@ -6,6 +6,7 @@ aea config set vendor.valory.skills.price_estimation_abci.models.randomness_api.
 aea config set vendor.valory.skills.price_estimation_abci.models.params.args.consensus.max_participants 4
 aea config set vendor.valory.skills.price_estimation_abci.models.params.args.keeper_timeout_seconds 5
 aea config set vendor.valory.skills.price_estimation_abci.models.params.args.tendermint_url http://node2:26657
+aea config set vendor.valory.skills.price_estimation_abci.models.params.args.observation_interval 600
 aea config set vendor.valory.connections.ledger.config.ledger_apis.ethereum.address  "https://ropsten.infura.io/v3/2980beeca3544c9fbace4f24218afcd4"
 aea config set vendor.valory.connections.ledger.config.ledger_apis.ethereum.chain_id  3
 aea build
