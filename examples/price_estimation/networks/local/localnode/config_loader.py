da3d196e32cd9bbc00091f0956a321f75398e165
8baller <8ball030@gmail.com>
2021-11-24 10:38:29 +0000

    added config as a seprate process.

---
+++ examples/price_estimation/networks/local/localnode/config_loader.py
@@ -0,0 +1,57 @@
+#! /usr/bin/python3
+from pathlib import Path
+import signal
+from glob import glob
+import time
+import os
+
+from argparse import ArgumentParser
+
+parser = ArgumentParser()
+parser.add_argument('-p', "--path_to_monitor",
+                    help='path to monitor for changes.',
+                    default="/app")
+parser.add_argument('-dp', "--destination_path",
+                    help="path for the configuration to be copied to",
+                    default="./out")
+parser.add_argument('-i', "--interval", default=1)
+
+import hashlib
+def md5(fname):
+    hash_md5 = hashlib.md5()
+    with open(fname, "rb") as f:
+        for chunk in iter(lambda: f.read(4096), b""):
+            hash_md5.update(chunk)
+    return hash_md5.hexdigest()
+
+
+def restart_host():
+    # This is a little inelegant, but ... effective.
+    data = [x.rstrip().strip() for x in os.popen('ps h -eo pid')][2:]
+    [os.kill(int(i), signal.SIGINT) for i in data]
+
+
+
+
+def hash_files(files):
+    return "".join([md5(file) for file in files])
+
+def main():
+    args = parser.parse_args()
+    files = glob(args.path_to_monitor + "/*")
+    state = hash_files(files)
+
+    print(f"Starting to track config folder. {args.path_to_monitor}")
+    print(f"Initial State: {state}")
+    while True:
+        files = glob(args.path_to_monitor + "/*")
+        new_state = hash_files(files)
+        if state != new_state:
+            print("new config detected")
+            restart_host()
+        time.sleep(1)
+        state = new_state
+
+if __name__ == '__main__':
+    main()
+
