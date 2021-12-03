472ac827704ff737fbaf3db623de51f31b059c56
David Minarsch <david.minarsch@googlemail.com>
2021-12-03 22:41:54 +0100

    fix: update dockerfile to py3.8 and header configs

--- examples/price_estimation/configure_agents/abci1.sh
+++ examples/price_estimation/configure_agents/abci1.sh
@@ -4,7 +4,7 @@ cp ../configure_agents/keys/ethereum_private_key_1.txt ethereum_private_key.txt
 
 aea config set vendor.valory.skills.price_estimation_abci.models.price_api.args.url https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest
 aea config set vendor.valory.skills.price_estimation_abci.models.price_api.args.api_id coinmarketcap
-aea config set vendor.valory.skills.price_estimation_abci.models.price_api.args.headers '[["Accepts","application/json"], ["X-CMC_PRO_API_KEY","27d2cd0d-80c3-4ec3-9305-4f3d9ad34e41"]]'  --type list
+aea config set vendor.valory.skills.price_estimation_abci.models.price_api.args.headers '[{"Accepts": "application/json"}, {"X-CMC_PRO_API_KEY": "27d2cd0d-80c3-4ec3-9305-4f3d9ad34e41"}]'  --type list
 aea config set vendor.valory.skills.price_estimation_abci.models.price_api.args.parameters '[["symbol","BTC"], ["convert","USD"]]'  --type list
 aea config set vendor.valory.skills.price_estimation_abci.models.price_api.args.response_key 'data:BTC:quote:USD:price'
 aea config set vendor.valory.skills.price_estimation_abci.models.randomness_api.args.url https://api.drand.sh/public/latest
