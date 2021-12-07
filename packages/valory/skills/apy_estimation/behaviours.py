d99432f1e216057762ecf956c2e5e42d1712d4b4
Adamantios <adamantios.zaras@valory.xyz>
2021-12-07 15:16:49 +0200

    [test]: create tests for `io` module

--- packages/valory/skills/apy_estimation/behaviours.py
+++ packages/valory/skills/apy_estimation/behaviours.py
@@ -763,7 +763,7 @@ class TrainBehaviour(APYEstimationBaseState):
                 save_path = os.path.join(
                     self.params.data_folder,
                     self.params.pair_id,
-                    f"{prefix}forecaster.pkl",
+                    f"{prefix}forecaster.joblib",
                 )
                 save_forecaster(save_path, forecaster)
 
@@ -807,7 +807,7 @@ class TestBehaviour(APYEstimationBaseState):
             y[split] = np.loadtxt(path, delimiter=",")
 
         model_path = os.path.join(
-            self.params.data_folder, self.params.pair_id, "forecaster.pkl"
+            self.params.data_folder, self.params.pair_id, "forecaster.joblib"
         )
         forecaster = load_forecaster(model_path)
 
@@ -887,7 +887,7 @@ class EstimateBehaviour(APYEstimationBaseState):
             model_path = os.path.join(
                 self.params.data_folder,
                 self.params.pair_id,
-                "fully_trained_forecaster.pkl",
+                "fully_trained_forecaster.joblib",
             )
             forecaster = load_forecaster(model_path)
             estimation = forecaster.predict(self.params.estimation["steps_forward"])
