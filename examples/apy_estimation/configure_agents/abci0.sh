f28de42ef2fcc5ba1ea743ebf501826004f1c120
Adamantios <adamantios.zaras@valory.xyz>
2021-12-13 16:09:06 +0200

    [fix]: correct configs

--- examples/apy_estimation/configure_agents/abci0.sh
+++ examples/apy_estimation/configure_agents/abci0.sh
@@ -4,10 +4,9 @@ cp ../configure_agents/keys/ethereum_private_key_0.txt ethereum_private_key.txt
 
 aea config set vendor.valory.skills.apy_estimation.models.spooky_subgraph.args.url https://api.thegraph.com/subgraphs/name/eerieeight/spookyswap
 aea config set vendor.valory.skills.apy_estimation.models.spooky_subgraph.args.api_id spookyswap
-aea config set vendor.valory.skills.apy_estimation.models.spooky_subgraph.args.response_key 'data'
+aea config set vendor.valory.skills.apy_estimation.models.spooky_subgraph.args.response_key "data"
 aea config set vendor.valory.skills.apy_estimation.models.spooky_subgraph.args.top_n_pools 100 --type int
 aea config set vendor.valory.skills.apy_estimation.models.spooky_subgraph.args.bundle_id 1 --type int
-aea config set vendor.valory.skills.apy_estimation.models.spooky_subgraph.args.headers "Content-Type: application/json"
 aea config set vendor.valory.skills.apy_estimation.models.randomness_api.args.url https://drand.cloudflare.com/public/latest
 aea config set vendor.valory.skills.apy_estimation.models.randomness_api.args.api_id cloudflare
 aea config set vendor.valory.skills.apy_estimation.models.fantom_subgraph.args.url https://api.thegraph.com/subgraphs/name/matthewlilley/fantom-blocks
@@ -23,12 +22,12 @@ aea config set vendor.valory.skills.apy_estimation.models.params.args.data_folde
 aea config set vendor.valory.skills.apy_estimation.models.params.args.drand_public_key 868f005eb8e6e4ca0a47c8a77ceaa5309a47978a7c71bc5cce96366b5d7a569937c529eeda66c7293784a9402801af31
 aea config set vendor.valory.skills.apy_estimation.models.params.args.estimation.steps_forward 1
 aea config set vendor.valory.skills.apy_estimation.models.params.args.history_duration 6
-aea config set vendor.valory.skills.apy_estimation.models.params.args.estimation.optimizer.n_trials 10 --type int
-aea config set vendor.valory.skills.apy_estimation.models.params.args.estimation.optimizer.timeout None
-aea config set vendor.valory.skills.apy_estimation.models.params.args.estimation.optimizer.n_jobs 1 --type int
-aea config set vendor.valory.skills.apy_estimation.models.params.args.estimation.optimizer.show_progress_bar false
-aea config set vendor.valory.skills.apy_estimation.models.params.args.estimation.optimizer.scoring pinball
-aea config set vendor.valory.skills.apy_estimation.models.params.args.estimation.optimizer.alpha 0.25
-aea config set vendor.valory.skills.apy_estimation.models.params.args.estimation.pair_id 0x2b4c76d0dc16be1c31d4c1dc53bf9b45987fc75c
-aea config set vendor.valory.skills.apy_estimation.models.params.args.estimation.sleep_time 10
+aea config set vendor.valory.skills.apy_estimation.models.params.args.optimizer.n_trials 10 --type int
+aea config set vendor.valory.skills.apy_estimation.models.params.args.optimizer.timeout None
+aea config set vendor.valory.skills.apy_estimation.models.params.args.optimizer.n_jobs 1 --type int
+aea config set vendor.valory.skills.apy_estimation.models.params.args.optimizer.show_progress_bar false
+aea config set vendor.valory.skills.apy_estimation.models.params.args.optimizer.scoring pinball
+aea config set vendor.valory.skills.apy_estimation.models.params.args.optimizer.alpha 0.25
+aea config set vendor.valory.skills.apy_estimation.models.params.args.pair_id 0x2b4c76d0dc16be1c31d4c1dc53bf9b45987fc75c
+aea config set vendor.valory.skills.apy_estimation.models.params.args.sleep_time 10
 aea build
