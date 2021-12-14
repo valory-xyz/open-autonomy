73c4e2f943e4afa4d62dda43222a3aace39309cb
David Minarsch <david.minarsch@googlemail.com>
2021-12-14 15:20:54 +0000

    refactor: extract end2end test class

--- tests/test_agents/test_simple_abci.py
+++ tests/test_agents/test_simple_abci.py
@@ -17,18 +17,9 @@
 #
 # ------------------------------------------------------------------------------
 
-"""Integration tests for the valory/simple_abci skill."""
-import json
-import logging
-from pathlib import Path
-from typing import List
+"""End2end tests for the valory/simple_abci skill."""
 
-from aea.test_tools.test_cases import AEATestCaseMany
-
-from tests.helpers.tendermint_utils import (
-    BaseTendermintTestClass,
-    TendermintLocalNetworkBuilder,
-)
+from tests.test_agents.base import BaseTestEnd2EndNormalExecution
 
 
 # check log messages of the happy path
@@ -50,136 +41,34 @@ CHECK_STRINGS = (
 )
 
 
-class BaseTestSimpleABCISkill(AEATestCaseMany, BaseTendermintTestClass):
-    """
-    Base class for simple ABCI skill tests.
-
-    The setup test function of this class will configure a set of 'n'
-    agents with the simple_abci skill, and a Tendermint network
-    of 'n' nodes, one for each agent.
-
-    Test subclasses must set NB_AGENTS.
-    """
-
-    NB_AGENTS: int
-    IS_LOCAL = True
-    capture_log = True
-    KEEPER_TIMEOUT = 10
-    cli_log_options = ["-v", "DEBUG"]
-    processes: List
-
-    def setup(self) -> None:
-        """Set up the test."""
-        self.agent_names = [f"agent_{i:05d}" for i in range(self.NB_AGENTS)]
-        self.processes = []
-        self.tendermint_net_builder = TendermintLocalNetworkBuilder(
-            self.NB_AGENTS, Path(self.t)
-        )
-
-        for agent_id, agent_name in enumerate(self.agent_names):
-            logging.debug(f"Processing agent {agent_name}...")
-            node = self.tendermint_net_builder.nodes[agent_id]
-            self.fetch_agent(
-                "valory/simple_abci:0.1.0", agent_name, is_local=self.IS_LOCAL
-            )
-            self.set_agent_context(agent_name)
-            self.generate_private_key("ethereum", "ethereum_private_key.txt")
-            self.add_private_key("ethereum", "ethereum_private_key.txt")
-            # each agent has its Tendermint node instance
-            self.set_config(
-                "vendor.valory.connections.abci.config.port",
-                node.abci_port,
-            )
-            self.set_config(
-                "vendor.valory.connections.abci.config.tendermint_config.rpc_laddr",
-                node.rpc_laddr,
-            )
-            self.set_config(
-                "vendor.valory.connections.abci.config.tendermint_config.p2p_laddr",
-                node.p2p_laddr,
-            )
-            self.set_config(
-                "vendor.valory.connections.abci.config.tendermint_config.home",
-                str(node.home),
-            )
-            self.set_config(
-                "vendor.valory.connections.abci.config.tendermint_config.p2p_seeds",
-                json.dumps(self.tendermint_net_builder.get_p2p_seeds()),
-                "list",
-            )
-            self.set_config(
-                "vendor.valory.skills.simple_abci.models.params.args.consensus.max_participants",
-                self.NB_AGENTS,
-            )
-            self.set_config(
-                "vendor.valory.skills.simple_abci.models.params.args.round_timeout_seconds",
-                self.KEEPER_TIMEOUT,
-            )
-            self.set_config(
-                "vendor.valory.skills.simple_abci.models.params.args.tendermint_url",
-                node.get_http_addr("localhost"),
-            )
-
-        # run 'aea install' in only one AEA project, to save time
-        self.set_agent_context(self.agent_names[0])
-        self.run_install()
-
-    def _launch_agent_i(self, i: int) -> None:
-        """Launch the i-th agent."""
-        agent_name = self.agent_names[i]
-        logging.debug(f"Launching agent {agent_name}...")
-        self.set_agent_context(agent_name)
-        process = self.run_agent()
-        self.processes.append(process)
-
-
-class BaseTestSimpleAbciSkillNormalExecution(BaseTestSimpleABCISkill):
-    """Test that the ABCI simple skill works together with Tendermint under normal circumstances."""
-
-    def test_run(self) -> None:
-        """Run the ABCI skill."""
-        for agent_id in range(self.NB_AGENTS):
-            self._launch_agent_i(agent_id)
-
-        logging.info("Waiting Tendermint nodes to be up")
-        self.health_check(
-            self.tendermint_net_builder, max_retries=20, sleep_interval=3.0
-        )
-
-        # check that *each* AEA prints these messages
-        for process in self.processes:
-            missing_strings = self.missing_from_output(process, CHECK_STRINGS, 40)
-            assert (
-                missing_strings == []
-            ), "Strings {} didn't appear in agent output.".format(missing_strings)
-
-            assert self.is_successfully_terminated(
-                process
-            ), "ABCI agent wasn't successfully terminated."
-
-
 class TestSimpleABCISingleAgent(
-    BaseTestSimpleAbciSkillNormalExecution,
-    BaseTendermintTestClass,
+    BaseTestEnd2EndNormalExecution,
 ):
     """Test that the ABCI simple_abci skill with only one agent."""
 
     NB_AGENTS = 1
+    agent_package = "valory/simple_abci:0.1.0"
+    wait_to_finish = 40
+    check_strings = CHECK_STRINGS
 
 
 class TestSimpleABCITwoAgents(
-    BaseTestSimpleAbciSkillNormalExecution,
-    BaseTendermintTestClass,
+    BaseTestEnd2EndNormalExecution,
 ):
     """Test that the ABCI simple_abci skill with two agents."""
 
     NB_AGENTS = 2
+    agent_package = "valory/simple_abci:0.1.0"
+    wait_to_finish = 40
+    check_strings = CHECK_STRINGS
 
 
 class TestSimpleABCIFourAgents(
-    BaseTestSimpleAbciSkillNormalExecution,
-    BaseTendermintTestClass,
+    BaseTestEnd2EndNormalExecution,
 ):
     """Test that the ABCI simple_abci skill with four agents."""
 
     NB_AGENTS = 4
+    agent_package = "valory/simple_abci:0.1.0"
+    wait_to_finish = 40
+    check_strings = CHECK_STRINGS
