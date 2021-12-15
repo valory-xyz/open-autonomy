805cfdeb6daeb924500a93b56515cb1f3beb3278
8baller <8ball030@gmail.com>
2021-12-15 18:18:18 +0000

    added tendermint reset functionality via HTTP api

--- examples/price_estimation/networks/local/localnode/app.py
+++ examples/price_estimation/networks/local/localnode/app.py
@@ -1,15 +1,24 @@
+from logging import log
 from flask import Flask
 from flask import Response
+import logging
+from tendermint import TendermintNode, TendermintParams
+
 app = Flask(__name__)
 
 @app.route("/gentle_reset")
 def gentle_reset():
+    tendermint_node.stop()
+    tendermint_node.start()
     return Response("Success",
                     status=200,
                     mimetype='application/json')
 
 @app.route("/hard_reset")
 def hard_reset():
+    tendermint_node.stop()
+    tendermint_node.prune_blocks()
+    tendermint_node.start()
     return Response("Success",
                     status=200,
                     mimetype='application/json')
@@ -30,6 +39,19 @@ def handle_server_error(e):
                     mimetype='application/json')
 
 
+import os
 
 if __name__ == "__main__":
+    id = f"{os.environ.get('ID', 1)}"
+    home = f"/tendermint/node{id}"
+
+    os.environ['TMHOME'] = home
+    
+    tendermint_params = TendermintParams(
+        proxy_app=f"tcp://abci{id}:26658",
+        consensus_create_empty_blocks=True,
+        home=home
+    )
+    tendermint_node = TendermintNode(tendermint_params)
+    tendermint_node.start()
     app.run()
