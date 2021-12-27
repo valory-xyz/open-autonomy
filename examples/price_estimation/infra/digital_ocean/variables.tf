9ef8098661ae74a018ed0589523fa5fa09641c7d
8ball030 <35799987+8ball030@users.noreply.github.com>
2021-12-27 21:10:36 +0000

    Update variables.tf

--- examples/price_estimation/infra/digital_ocean/variables.tf
+++ examples/price_estimation/infra/digital_ocean/variables.tf
@@ -17,10 +17,7 @@ variable "ssh_fingerprint"{
     description = "fingerprint of the sshkey to be used to control the newly created workers."
     default = "12:31:25:bc:4e:d4:e8:e4:74:34:b1:9d:e2:f8:49:b2"
 }
-# variable "ssh_pub_key"{
-#     description = "ssh public key path of the controlling key"
-#     default ="${chomp(file("~/.config/digital-ocean/token"))}" 
-# }
+
 variable "cred_file"{
     description = "path of credentials for Digital Ocean"
     default = "../do_token"
