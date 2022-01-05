322c746823b49043df59498fabcdefd06f4ea2f8
David Vilela <dvilelaf@gmail.com>
2022-01-05 16:06:50 +0100

    fix: update copyright script

--- scripts/check_copyright.py
+++ scripts/check_copyright.py
@@ -33,30 +33,31 @@ import itertools
 import re
 import sys
 from pathlib import Path
-
+from datetime import datetime
+import os
 
 HEADER_REGEX = re.compile(
     r"""(#!/usr/bin/env python3
 )?# -\*- coding: utf-8 -\*-
 # ------------------------------------------------------------------------------
 #
-#   Copyright 2021 (Valory AG|valory)
+#   Copyright (2021(-202\d)?) Valory AG
 #
-#   Licensed under the Apache License, Version 2\.0 \(the \"License\"\);
+#   Licensed under the Apache License, Version 2\.0 \(the "License"\);
 #   you may not use this file except in compliance with the License\.
 #   You may obtain a copy of the License at
 #
-#       http://www\.apache\.org/licenses/LICENSE-2\.0
+#       http:\/\/www\.apache\.org\/licenses\/LICENSE-2\.0
 #
 #   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an \"AS IS\" BASIS,
+#   distributed under the License is distributed on an "AS IS" BASIS,
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied\.
 #   See the License for the specific language governing permissions and
 #   limitations under the License\.
 #
 # ------------------------------------------------------------------------------
 """,
-    re.MULTILINE,
+    re.MULTILINE
 )
 
 
@@ -71,7 +72,30 @@ def check_copyright(file: Path) -> bool:
     :return: True if the file is compliant with the checks, False otherwise.
     """
     content = file.read_text()
-    return re.match(HEADER_REGEX, content) is not None
+
+    match = re.match(HEADER_REGEX, content)
+
+    # No match
+    if match is None:
+        return False
+
+    copyright_years_str = match.groups(0)[1]
+    copyright_years = tuple(int(i) for i in copyright_years_str.split("-"))
+    modification_date = datetime.fromtimestamp(os.path.getmtime(file))
+
+    # Start year is not 2021
+    if copyright_years[0] != 2021:
+        return False
+
+    # Start year is 2021 but the file has been modified in another later year (missing -202x)
+    if len(copyright_years) == 1 and copyright_years[0] != modification_date.year:
+        return False
+
+    # End year does not match the last modification year
+    if len(copyright_years) > 1 and copyright_years[1] != modification_date.year:
+        return False
+
+    return True
 
 
 if __name__ == "__main__":
