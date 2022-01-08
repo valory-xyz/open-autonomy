3d5618742c261bd43432807b2fca823b0396d717
David Minarsch <david.minarsch@googlemail.com>
2022-01-08 11:34:13 +0000

    chore: update outdated copyright headers

--- scripts/check_copyright.py
+++ scripts/check_copyright.py
@@ -117,9 +117,13 @@ if __name__ == "__main__":
         lambda x: x not in exclude_files,
         itertools.chain(
             Path("aea_consensus_algorithm").glob("**/*.py"),
+            Path("benchmark").glob("**/*.py"),
+            Path("examples").glob("**/*.py"),
+            Path("tests").glob("**/*.py"),
             Path("packages", "valory", "agents").glob("**/*.py"),
             Path("packages", "valory", "connections").glob("**/*.py"),
             Path("packages", "valory", "contracts").glob("**/*.py"),
+            Path("packages", "valory", "skills").glob("**/*.py"),
             Path("tests").glob("**/*.py"),
             Path("scripts").glob("**/*.py"),
         ),
