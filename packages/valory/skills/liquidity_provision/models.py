c7bcc8db0e6ea1fc8f3fd61f2a3097b59d786a47
David Vilela <dvilelaf@gmail.com>
2022-01-29 20:01:30 +0100

    fix: add event setup to composition
    [skip ci]

--- packages/valory/skills/liquidity_provision/models.py
+++ packages/valory/skills/liquidity_provision/models.py
@@ -29,7 +29,13 @@ from packages.valory.skills.abstract_round_abci.models import (
 from packages.valory.skills.liquidity_provision.composition import (
     LiquidityProvisionAbciApp,
 )
+from packages.valory.skills.price_estimation_abci.rounds import Event
+from packages.valory.skills.safe_deployment_abci.rounds import Event as SafeEvent
+from packages.valory.skills.transaction_settlement_abci.rounds import Event as TSEvent
 
+
+MARGIN = 5
+MULTIPLIER = 2
 
 Requests = BaseRequests
 
@@ -40,6 +46,33 @@ class SharedState(BaseSharedState):
     def __init__(self, *args: Any, **kwargs: Any) -> None:
         """Initialize the state."""
         super().__init__(*args, abci_app_cls=LiquidityProvisionAbciApp, **kwargs)
+
+    def setup(self) -> None:
+        """Set up."""
+        super().setup()
+        LiquidityProvisionAbciApp.event_to_timeout[
+            Event.ROUND_TIMEOUT
+        ] = self.context.params.round_timeout_seconds
+        LiquidityProvisionAbciApp.event_to_timeout[
+            SafeEvent.ROUND_TIMEOUT
+        ] = self.context.params.round_timeout_seconds
+        LiquidityProvisionAbciApp.event_to_timeout[
+            TSEvent.ROUND_TIMEOUT
+        ] = self.context.params.round_timeout_seconds
+        LiquidityProvisionAbciApp.event_to_timeout[TSEvent.RESET_TIMEOUT] = (
+            self.context.params.round_timeout_seconds * MULTIPLIER
+        )
+        LiquidityProvisionAbciApp.event_to_timeout[SafeEvent.VALIDATE_TIMEOUT] = (
+            self.context.params.retry_timeout * self.context.params.retry_attempts
+            + MARGIN
+        )
+        LiquidityProvisionAbciApp.event_to_timeout[TSEvent.VALIDATE_TIMEOUT] = (
+            self.context.params.retry_timeout * self.context.params.retry_attempts
+            + MARGIN
+        )
+        LiquidityProvisionAbciApp.event_to_timeout[SafeEvent.DEPLOY_TIMEOUT] = (
+            self.context.params.keeper_timeout + MARGIN
+        )
 
 
 class Params(BaseParams):
