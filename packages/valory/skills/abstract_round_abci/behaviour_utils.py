4be49f7134689faa123d1ee59c2a9f37f4046a3d
David Minarsch <david.minarsch@googlemail.com>
2022-01-31 10:47:31 +0000

    fix: extend max retries in abstract round abci

--- packages/valory/skills/abstract_round_abci/behaviour_utils.py
+++ packages/valory/skills/abstract_round_abci/behaviour_utils.py
@@ -82,7 +82,7 @@ from packages.valory.skills.abstract_round_abci.models import (
 _DEFAULT_REQUEST_RETRY_DELAY = 1.0
 _DEFAULT_REQUEST_TIMEOUT = 10.0
 _DEFAULT_TX_TIMEOUT = 10.0
-_DEFAULT_TX_MAX_ATTEMPTS = 5
+_DEFAULT_TX_MAX_ATTEMPTS = 10
 
 _SYNC_MODE_WAIT = 3
 
