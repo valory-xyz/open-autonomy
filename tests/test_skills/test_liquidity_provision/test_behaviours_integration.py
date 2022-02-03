b42b063964907ec9c316079c1f8bf95683b15513
David Vilela <dvilelaf@gmail.com>
2022-02-03 17:38:54 +0100

    test: refactor integration tests

--- tests/test_skills/test_liquidity_provision/test_behaviours_integration.py
+++ tests/test_skills/test_liquidity_provision/test_behaviours_integration.py
@@ -106,6 +106,7 @@ from packages.valory.skills.transaction_settlement_abci.rounds import (
 from tests.conftest import ROOT_DIR, make_ledger_api_connection
 from tests.fixture_helpers import HardHatAMMBaseTest
 from tests.helpers.contracts import get_register_contract
+from tests.test_skills.base import FSMBehaviourBaseCase
 
 
 DEFAULT_GAS = 1000000
@@ -183,202 +184,20 @@ def merge_transfer_strings(transfer_strings: List[str]) -> str:
     return json.dumps(dict(transfers=transfers))
 
 
-class LiquidityProvisionBehaviourBaseCase(BaseSkillTestCase):
+class LiquidityProvisionBehaviourBaseCase(FSMBehaviourBaseCase):
     """Base case for testing LiquidityProvision FSMBehaviour."""
 
     path_to_skill = Path(
         ROOT_DIR, "packages", "valory", "skills", "liquidity_provision"
     )
 
-    liquidity_provision_behaviour: LiquidityProvisionConsensusBehaviour
+    behaviour: LiquidityProvisionConsensusBehaviour
     ledger_handler: LedgerApiHandler
     http_handler: HttpHandler
     contract_handler: ContractApiHandler
     signing_handler: SigningHandler
     old_tx_type_to_payload_cls: Dict[str, Type[BaseTxPayload]]
     period_state: LiquidityProvisionPeriodState
-
-    @classmethod
-    def setup(cls, **kwargs: Any) -> None:
-        """Setup the test class."""
-        # we need to store the current value of the meta-class attribute
-        # _MetaPayload.transaction_type_to_payload_cls, and restore it
-        # in the teardown function. We do a shallow copy so we avoid
-        # to modify the old mapping during the execution of the tests.
-        cls.old_tx_type_to_payload_cls = copy(
-            _MetaPayload.transaction_type_to_payload_cls
-        )
-        _MetaPayload.transaction_type_to_payload_cls = {}
-        super().setup()
-        assert cls._skill.skill_context._agent_context is not None
-        cls._skill.skill_context._agent_context.identity._default_address_key = (
-            "ethereum"
-        )
-        cls._skill.skill_context._agent_context._default_ledger_id = "ethereum"
-        cls.liquidity_provision_behaviour = cast(
-            LiquidityProvisionConsensusBehaviour,
-            cls._skill.skill_context.behaviours.main,
-        )
-        cls.http_handler = cast(HttpHandler, cls._skill.skill_context.handlers.http)
-        cls.signing_handler = cast(
-            SigningHandler, cls._skill.skill_context.handlers.signing
-        )
-        cls.contract_handler = cast(
-            ContractApiHandler, cls._skill.skill_context.handlers.contract_api
-        )
-        cls.ledger_handler = cast(
-            LedgerApiHandler, cls._skill.skill_context.handlers.ledger_api
-        )
-
-        cls.liquidity_provision_behaviour.setup()
-        cls._skill.skill_context.state.setup()
-        assert (
-            cast(BaseState, cls.liquidity_provision_behaviour.current_state).state_id
-            == cls.liquidity_provision_behaviour.initial_state_cls.state_id
-        )
-        cls.period_state = LiquidityProvisionPeriodState(
-            StateDB(initial_period=0, initial_data={})
-        )
-
-    def fast_forward_to_state(
-        self,
-        behaviour: AbstractRoundBehaviour,
-        state_id: str,
-        period_state: BasePeriodState,
-    ) -> None:
-        """Fast forward the FSM to a state."""
-        next_state = {s.state_id: s for s in behaviour.behaviour_states}[state_id]
-        assert next_state is not None, f"State {state_id} not found"
-        next_state = cast(Type[BaseState], next_state)
-        behaviour.current_state = next_state(
-            name=next_state.state_id, skill_context=behaviour.context
-        )
-        self.skill.skill_context.state.period.abci_app._round_results.append(
-            period_state
-        )
-        if next_state.matching_round is not None:
-            self.skill.skill_context.state.period.abci_app._current_round = (
-                next_state.matching_round(
-                    period_state, self.skill.skill_context.params.consensus_params
-                )
-            )
-
-    def mock_ledger_api_request(
-        self, request_kwargs: Dict, response_kwargs: Dict
-    ) -> None:
-        """
-        Mock http request.
-
-        :param request_kwargs: keyword arguments for request check.
-        :param response_kwargs: keyword arguments for mock response.
-        """
-
-        self.assert_quantity_in_outbox(1)
-        actual_ledger_api_message = self.get_message_from_outbox()
-        assert actual_ledger_api_message is not None, "No message in outbox."
-        has_attributes, error_str = self.message_has_attributes(
-            actual_message=actual_ledger_api_message,
-            message_type=LedgerApiMessage,
-            to=str(LEDGER_CONNECTION_PUBLIC_ID),
-            sender=str(self.skill.skill_context.skill_id),
-            **request_kwargs,
-        )
-
-        assert has_attributes, error_str
-        incoming_message = self.build_incoming_message(
-            message_type=LedgerApiMessage,
-            dialogue_reference=(
-                actual_ledger_api_message.dialogue_reference[0],
-                "stub",
-            ),
-            target=actual_ledger_api_message.message_id,
-            message_id=-1,
-            to=str(self.skill.skill_context.skill_id),
-            sender=str(LEDGER_CONNECTION_PUBLIC_ID),
-            ledger_id=str(LEDGER_CONNECTION_PUBLIC_ID),
-            **response_kwargs,
-        )
-        self.ledger_handler.handle(incoming_message)
-        self.liquidity_provision_behaviour.act_wrapper()
-
-    def mock_contract_api_request(
-        self, contract_id: str, request_kwargs: Dict, response_kwargs: Dict
-    ) -> None:
-        """
-        Mock http request.
-
-        :param contract_id: contract id.
-        :param request_kwargs: keyword arguments for request check.
-        :param response_kwargs: keyword arguments for mock response.
-        """
-
-        self.assert_quantity_in_outbox(1)
-        actual_contract_ledger_message = self.get_message_from_outbox()
-        assert actual_contract_ledger_message is not None, "No message in outbox."
-        has_attributes, error_str = self.message_has_attributes(
-            actual_message=actual_contract_ledger_message,
-            message_type=ContractApiMessage,
-            to=str(LEDGER_CONNECTION_PUBLIC_ID),
-            sender=str(self.skill.skill_context.skill_id),
-            ledger_id="ethereum",
-            contract_id=contract_id,
-            message_id=1,
-            **request_kwargs,
-        )
-        assert has_attributes, error_str
-        self.liquidity_provision_behaviour.act_wrapper()
-
-        incoming_message = self.build_incoming_message(
-            message_type=ContractApiMessage,
-            dialogue_reference=(
-                actual_contract_ledger_message.dialogue_reference[0],
-                "stub",
-            ),
-            target=actual_contract_ledger_message.message_id,
-            message_id=-1,
-            to=str(self.skill.skill_context.skill_id),
-            sender=str(LEDGER_CONNECTION_PUBLIC_ID),
-            ledger_id="ethereum",
-            contract_id=str(GNOSIS_SAFE_CONTRACT_ID),
-            **response_kwargs,
-        )
-        self.contract_handler.handle(incoming_message)
-        self.liquidity_provision_behaviour.act_wrapper()
-
-    def mock_http_request(self, request_kwargs: Dict, response_kwargs: Dict) -> None:
-        """
-        Mock http request.
-
-        :param request_kwargs: keyword arguments for request check.
-        :param response_kwargs: keyword arguments for mock response.
-        """
-
-        self.assert_quantity_in_outbox(1)
-        actual_http_message = self.get_message_from_outbox()
-        assert actual_http_message is not None, "No message in outbox."
-        has_attributes, error_str = self.message_has_attributes(
-            actual_message=actual_http_message,
-            message_type=HttpMessage,
-            performative=HttpMessage.Performative.REQUEST,
-            to=str(HTTP_CLIENT_PUBLIC_ID),
-            sender=str(self.skill.skill_context.skill_id),
-            **request_kwargs,
-        )
-        assert has_attributes, error_str
-        self.liquidity_provision_behaviour.act_wrapper()
-        self.assert_quantity_in_outbox(0)
-        incoming_message = self.build_incoming_message(
-            message_type=HttpMessage,
-            dialogue_reference=(actual_http_message.dialogue_reference[0], "stub"),
-            performative=HttpMessage.Performative.RESPONSE,
-            target=actual_http_message.message_id,
-            message_id=-1,
-            to=str(self.skill.skill_context.skill_id),
-            sender=str(HTTP_CLIENT_PUBLIC_ID),
-            **response_kwargs,
-        )
-        self.http_handler.handle(incoming_message)
-        self.liquidity_provision_behaviour.act_wrapper()
 
     def mock_signing_request(self, request_kwargs: Dict, response_kwargs: Dict) -> None:
         """Mock signing request."""
@@ -403,100 +222,7 @@ class LiquidityProvisionBehaviourBaseCase(BaseSkillTestCase):
             **response_kwargs,
         )
         self.signing_handler.handle(incoming_message)
-        self.liquidity_provision_behaviour.act_wrapper()
-
-    def mock_a2a_transaction(
-        self,
-    ) -> None:
-        """Performs mock a2a transaction."""
-
-        self.mock_signing_request(
-            request_kwargs=dict(
-                performative=SigningMessage.Performative.SIGN_MESSAGE,
-            ),
-            response_kwargs=dict(
-                performative=SigningMessage.Performative.SIGNED_MESSAGE,
-                signed_message=SignedMessage(
-                    ledger_id="ethereum", body="stub_signature"
-                ),
-            ),
-        )
-
-        self.mock_http_request(
-            request_kwargs=dict(
-                method="GET",
-                headers="",
-                version="",
-                body=b"",
-            ),
-            response_kwargs=dict(
-                version="",
-                status_code=200,
-                status_text="",
-                headers="",
-                body=json.dumps({"result": {"hash": "", "code": OK_CODE}}).encode(
-                    "utf-8"
-                ),
-            ),
-        )
-        self.mock_http_request(
-            request_kwargs=dict(
-                method="GET",
-                headers="",
-                version="",
-                body=b"",
-            ),
-            response_kwargs=dict(
-                version="",
-                status_code=200,
-                status_text="",
-                headers="",
-                body=json.dumps({"result": {"tx_result": {"code": OK_CODE}}}).encode(
-                    "utf-8"
-                ),
-            ),
-        )
-
-    def end_round(
-        self,
-    ) -> None:
-        """Ends round early to cover `wait_for_end` generator."""
-        current_state = cast(
-            BaseState, self.liquidity_provision_behaviour.current_state
-        )
-        if current_state is None:
-            return
-        current_state = cast(BaseState, current_state)
-        if current_state.matching_round is None:
-            return
-        abci_app = current_state.context.state.period.abci_app
-        old_round = abci_app._current_round
-        abci_app._last_round = old_round
-        abci_app._current_round = abci_app.transition_function[
-            current_state.matching_round
-        ][Event.DONE](abci_app.state, abci_app.consensus_params)
-        abci_app._previous_rounds.append(old_round)
-        self.liquidity_provision_behaviour._process_current_round()
-
-    def _test_done_flag_set(self) -> None:
-        """Test that, when round ends, the 'done' flag is set."""
-        current_state = cast(
-            BaseState, self.liquidity_provision_behaviour.current_state
-        )
-        assert not current_state.is_done()
-        with mock.patch.object(
-            self.liquidity_provision_behaviour.context.state, "period"
-        ) as mock_period:
-            mock_period.last_round_id = cast(
-                AbstractRound, current_state.matching_round
-            ).round_id
-            current_state.act_wrapper()
-            assert current_state.is_done()
-
-    @classmethod
-    def teardown(cls) -> None:
-        """Teardown the test class."""
-        _MetaPayload.transaction_type_to_payload_cls = cls.old_tx_type_to_payload_cls  # type: ignore
+        self.behaviour.act_wrapper()
 
 
 class TestLiquidityProvisionHardhat(
@@ -773,7 +499,7 @@ class TestLiquidityProvisionHardhat(
         :param expected_types: the types to be expected
         :return: the incoming message
         """
-        self.liquidity_provision_behaviour.act_wrapper()
+        self.behaviour.act_wrapper()
 
         if type(handler) == SigningHandler:
             self.assert_quantity_in_decision_making_queue(1)
@@ -855,14 +581,14 @@ class TestLiquidityProvisionHardhat(
         ), "Number of cycles, handlers, contents and types does not match"
 
         self.fast_forward_to_state(
-            behaviour=self.liquidity_provision_behaviour,
+            behaviour=self.behaviour,
             state_id=state_id,
             period_state=period_state,
         )
         assert (
             cast(
                 BaseState,
-                cast(BaseState, self.liquidity_provision_behaviour.current_state),
+                cast(BaseState, self.behaviour.current_state),
             ).state_id
             == state_id
         )
@@ -874,7 +600,7 @@ class TestLiquidityProvisionHardhat(
             )
             incoming_messages.append(incoming_message)
 
-        self.liquidity_provision_behaviour.act_wrapper()
+        self.behaviour.act_wrapper()
         self.mock_a2a_transaction()
         return tuple(incoming_messages)
 
