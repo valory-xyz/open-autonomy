56fb51d2b2afab03aa044ae5a233f7dc4e569764
8baller <8ball030@gmail.com>
2022-02-04 16:57:46 +0000

    feat: deleted unnecessary constants

--- deployments/constants.py
+++ deployments/constants.py
@@ -51,58 +51,6 @@ KEYS: List[str] = [
     "0xdf57089febbacf7ba0bc227dafbffa9fc08a93fdc68e1e42411a14efcf23656e",
 ]
 
-RANDOMNESS_APIS: List[List[Tuple[str, str]]] = [
-    [
-        ("url", "https://drand.cloudflare.com/public/latest"),
-        ("api_id", "cloudflare"),
-    ],
-    [
-        ("url", "https://api.drand.sh/public/latest"),
-        ("api_id", "protocollabs1"),
-    ],
-    [
-        ("url", "https://api2.drand.sh/public/latest"),
-        ("api_id", "protocollabs2"),
-    ],
-    [
-        ("url", "https://api3.drand.sh/public/latest"),
-        ("api_id", "protocollabs3"),
-    ],
-]
-
-PRICE_APIS: List[List[Tuple[str, str]]] = [
-    [
-        ("url", "https://api.coingecko.com/api/v3/simple/price"),
-        ("api_id", "coingecko"),
-        (
-            "parameters",
-            """[["ids", "bitcoin"],["vs_currencies", "usd"]]""",
-        ),
-        ("response_key", "'bitcoin:usd'"),
-    ],
-    [
-        ("url", "https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest"),
-        ("api_id", "coinmarketcap"),
-        (
-            "headers",
-            """[["Accepts","application/json"], ["X-CMC_PRO_API_KEY","2142662b-985c-4862-82d7-e91457850c2a"]]""",
-        ),
-        ("parameters", """[["symbol","BTC"], ["convert","USD"]]"""),
-        ("response_key", "'data:BTC:quote:USD:price'"),
-    ],
-    [
-        ("url", "https://api.coinbase.com/v2/prices/BTC-USD/buy"),
-        ("api_id", "coinbase"),
-        ("response_key", "'data:amount'"),
-    ],
-    [
-        ("url", "https://api.binance.com/api/v3/ticker/price"),
-        ("api_id", "binance"),
-        ("parameters", """[["symbol", "BTCUSDT"]]"""),
-        ("response_key", "price"),
-    ],
-]
-
 
 NETWORKS = {
     "hardhat": {
