343132497e45252e51e01fc8b513ba53aa73c89a
m.a.p.karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-02-07 16:48:58 +0100

    fix:server-local

--- examples/price_estimation/server.py
+++ examples/price_estimation/server.py
@@ -22,16 +22,27 @@
 from typing import Optional
 from http import HTTPStatus
 from datetime import datetime
-import json
-import asyncio
-import websockets
-from flask import Flask, request, jsonify
+
+import random
+import time
+
+from threading import Thread, Event
+
+from flask import Flask, request, jsonify, render_template
+from flask_socketio import SocketIO, emit, send
+from flask_cors import CORS
+
 from packages.valory.skills.abstract_round_abci.serializer import (
     DictProtobufStructSerializer,
 )
 
+# setup
 app = Flask(__name__)
+# app.config['SECRET_KEY'] = 'private_key'
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
+# constants and storage
 PORT = 9999  # must match skill.yaml file specification
 period_data = {}
 data_sources = {}
@@ -39,8 +50,8 @@ last_period_count: Optional[int] = None
 
 
 dummy_period_data = {
-    "-1":
-        {
+    "-1": {
+        "0x_address_agent1": {
             "estimate": 6.66,
             "observations": {
                 "0x_address_agent1": 0.0,
@@ -50,15 +61,13 @@ dummy_period_data = {
             },
             "signature": "tx_hash",
             "time_stamp": "2022-02-03T10:54:35.233373",
+            "data_source": "coinmarketcap",
             "unit": "BTC:USD",
         },
-}
-
-dummy_data_sources = {
-    "0x_address_agent1": "coingecko",
-    "0x_address_agent2": "coinmarketcap",
-    "0x_address_agent3": "coinbase",
-    "0x_address_agent4": "binance",
+        "0x_address_agent2": {},
+        "0x_address_agent3": {},
+        "0x_address_agent4": {},
+    }
 }
 
 
@@ -66,12 +75,6 @@ dummy_data_sources = {
 def data():
     """API endpoint for GET requests"""
     return period_data if period_data else dummy_period_data
-
-
-@app.route("/sources")
-def sources():
-    """API endpoint for GET requests"""
-    return data_sources if data_sources else dummy_data_sources
 
 
 @app.route("/deposit", methods=['POST'])
@@ -90,6 +93,181 @@ def deposit() -> int:
         return HTTPStatus.BAD_REQUEST
 
 
+thread = Thread()
+thread_stop_event = Event()
+
+
+def generate_random_number():
+    while not thread_stop_event.is_set():
+        number = round(random.random(), 3)
+        socketio.emit('newnumber', {'number': number}, namespace='/test')
+        socketio.sleep(5)
+
+
+def generate_random_period_data():
+    while not thread_stop_event.is_set():
+        dummy_period_data["-1"]["0x_address_agent1"]["estimate"] = random.random()
+        socketio.emit('new_data', {'period': dummy_period_data}, namespace='/test')
+        socketio.sleep(5)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@socketio.on('connect', namespace='/test')
+def test_connect():
+    global thread  # need visibility of the global thread object
+    if not thread.is_alive():
+        thread = socketio.start_background_task(generate_random_period_data)
+
+
+@socketio.on('disconnect', namespace='/test')
+def test_disconnect():
+    print('Client disconnected')
+
+
+# @socketio.on('message')
+# def handle_message(message):
+#     send(message, broadcast=True)
+#
+#
+# @socketio.on('json')
+# def handle_json(json):
+#     send(json, json=True)
+
+# import random, time
+# thread = Thread()
+# thread_stop_event = Event()
+#
+#
+# def randomNumberGenerator():
+#     """
+#     Generate a random number every 1 second and emit to a socketio instance (broadcast)
+#     Ideally to be run in a separate thread?
+#     """
+#     #infinite loop of magical random numbers
+#     print("Making random numbers")
+#     while not thread_stop_event.isSet():
+#         number = round(random.random()*10, 3)
+#         print(number)
+#         socketio.emit('newnumber', {'number': number}, namespace='/test')
+#         socketio.sleep(5)
+#
+#
+# @app.route('/')
+# def index():
+#     #only by sending this page first will the client be connected to the socketio instance
+#     return render_template('index.html')
+#
+#
+# @socketio.on('connect', namespace='/test')
+# def test_connect():
+#     # need visibility of the global thread object
+#     global thread
+#     print('Client connected')
+#     if not thread.isAlive():
+#         print("Starting Thread")
+#         thread = socketio.start_background_task(randomNumberGenerator)
+#
+#
+# @socketio.on('disconnect', namespace='/test')
+# def test_disconnect():
+#     print('Client disconnected')
+
+
+# class RandomThread(Thread):
+#
+#     def __init__(self):
+#         super(RandomThread, self).__init__()
+#
+#     def randomNumberGenerator(self):
+#         """
+#         Generate a random number every 1 second and emit to a socketio instance (broadcast)
+#         Ideally to be run in a separate thread?
+#         """
+#         #infinite loop of magical random numbers
+#         print("Making random numbers")
+#         while not thread_stop_event.isSet():
+#             number = round(random.random()*10, 3)
+#             print(number)
+#             socketio.emit('newnumber', {'number': number}, namespace='/test')
+#             time.sleep(1)
+#
+#     def run(self):
+#         self.randomNumberGenerator()
+#
+#
+# @app.route('/')
+# def index():
+#     # only by sending this page first will the client be connected to the socketio instance
+#     import os
+#     return render_template('index.html')
+#
+#
+# @socketio.on('connect', namespace='/test')
+# def test_connect():
+#     # need visibility of the global thread object
+#     global thread
+#     print('Client connected')
+#     if not thread.is_alive():
+#         print("Starting Thread")
+#         thread = RandomThread()
+#         thread.start()
+#
+#
+# @socketio.on('disconnect', namespace='/test')
+# def test_disconnect():
+#     print('Client disconnected')
+
+
+
+
+# @socketio.on('connect')
+# def test_connect():
+#     emit('responseMessage', {'data': 'Connected! ayy'})
+    # need visibility of the global thread object
+    # global thread
+    # if not thread.isAlive():
+    #     print("Starting Thread")
+    #     thread = DataThread()
+    #     thread.start()
+
+# @socketio.on('message')
+# def handle_message(msg):
+#     pass
+
+
+
+# @app.route("/pulse", methods=['GET', 'POST'])
+# def pulse():
+#     """API endpoint for POST requests"""
+#
+#     import time
+#     import requests
+#
+#     url = 'https://1359-62-131-191-3.ngrok.io/test'
+#     headers = {'Content-type': 'text/html; charset=UTF-8'}
+#     response = requests.post(url, data=data, headers=headers)
+#     # wait for the response. it should not be higher
+#     # than keep alive time for TCP connection
+#
+#     # render template or redirect to some url:
+#     # return redirect("some_url")
+#     # return render_template("some_page.html", message=str(response.text)) # or response.json()
+#
+#     i = 0
+#     while True:
+#         i += 1
+#         time.sleep(3)
+#         return i
+#     # return "Content not supported"
+
+
+
+
 if __name__ == '__main__':
     host = "0.0.0.0"
     app.run(host=host, port=PORT, debug=True)
+
