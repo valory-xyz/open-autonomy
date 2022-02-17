b6b8b63fa90872f0bdd40de857b8ecc310a7713d
Adamantios <adamantios.zaras@valory.xyz>
2022-02-17 16:55:41 +0200

    test: `PreprocessPayload`'s hashes args are now optional

--- tests/test_skills/test_apy_estimation/test_payloads.py
+++ tests/test_skills/test_apy_estimation/test_payloads.py
@@ -86,12 +86,6 @@ class TestPayloads:
     @staticmethod
     def test_preprocess_payload() -> None:
         """Test `PreprocessPayload`"""
-        with pytest.raises(
-            ValueError,
-            match="Either `train_hash` and `test_hash` or `train_test` should be given for the `PreprocessPayload`!",
-        ):
-            PreprocessPayload(sender="sender", pair_name="test")
-
         payload = PreprocessPayload(
             sender="sender", pair_name="test", train_test="x0", id_="id"
         )
