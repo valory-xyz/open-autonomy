bc6ebb22011d602ee1c69ca38f74709e84e9b5e4
David Minarsch <david.minarsch@googlemail.com>
2022-02-18 20:39:20 +0100

    fix: find ip

--- deployments/constants.py
+++ deployments/constants.py
@@ -25,8 +25,18 @@ from string import Template
 from typing import Any, Dict, List
 
 
-hostname = socket.gethostname()
-host_ip_address = socket.gethostbyname(hostname + ".local")
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.settimeout(0)
+    try:
+        # doesn't even have to be reachable
+        s.connect(('10.255.255.255', 1))
+        IP = s.getsockname()[0]
+    except Exception:
+        IP = '127.0.0.1'
+    finally:
+        s.close()
+    return IP
 
 
 KEYS: List[str] = [
@@ -56,7 +66,7 @@ KEYS: List[str] = [
 NETWORKS = {
     "docker-compose": {
         "hardhat": {
-            "LEDGER_ADDRESS": f"http://{host_ip_address}:8545",
+            "LEDGER_ADDRESS": f"http://{get_ip()}:8545",
             "LEDGER_CHAIN_ID": 31337,
         },
         "ropsten": {
