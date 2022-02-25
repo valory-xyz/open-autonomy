99c617eb3f2c9f7eae920def383453c24a61568f
Adamantios <adamantios.zaras@valory.xyz>
2022-02-25 18:34:20 +0200

    test: add test for `group_and_filter_pair_data`

--- tests/test_skills/test_apy_estimation/ml/test_preprocessing.py
+++ tests/test_skills/test_apy_estimation/ml/test_preprocessing.py
@@ -24,11 +24,43 @@ import pytest
 
 from packages.valory.skills.apy_estimation_abci.ml.preprocessing import (
     prepare_pair_data,
+    group_and_filter_pair_data,
 )
 
 
 class TestPreprocessing:
     """Tests for preprocessing operations."""
+
+    @staticmethod
+    def test_group_and_filter_pair_data(
+        transformed_historical_data: pd.DataFrame,
+    ) -> None:
+        """Test `group_and_filter_pair_data`."""
+        # test with too few observations.
+        with pytest.raises(ValueError, match="Cannot work with < 5 observations for: "):
+            not_enough_data = transformed_historical_data[
+                transformed_historical_data["id"].isin(["x2", "x3"])
+            ]
+            group_and_filter_pair_data(not_enough_data)
+
+        # test with correct number of data.
+        id_with_enough_observations = "0x2b4c76d0dc16be1c31d4c1dc53bf9b45987fc75c"
+        pool1_data = transformed_historical_data.loc[
+            transformed_historical_data["id"] == id_with_enough_observations
+        ]
+        pool2_data = pool1_data.copy()
+        pool2_data["id"] = "test_id"
+        test_data = pd.concat([pool1_data, pool2_data])
+        grouped_and_filtered = group_and_filter_pair_data(test_data)
+
+        filtering_columns = ["blockTimestamp", "APY", "pairName"]
+        pd.testing.assert_frame_equal(
+            grouped_and_filtered.get_group(id_with_enough_observations),
+            pool1_data[filtering_columns],
+        )
+        pd.testing.assert_frame_equal(
+            grouped_and_filtered.get_group("test_id"), pool2_data[filtering_columns]
+        )
 
     def test_prepare_pair_data(self, transformed_historical_data: pd.DataFrame) -> None:
         """Test `prepare_pair_data`."""
