aea5183a9e9e63575828dd1bbec15631b898c113
Viraj Patel <vptl185@gmail.com>
2022-03-04 07:16:04 +0000

    feat: added start script for dev mode

---
+++ deployments/Dockerfiles/open_aea/start_dev.sh
@@ -0,0 +1,7 @@
+sudo chown -R ubuntu:ubuntu /logs
+sudo chown -R ubuntu:ubuntu /home/ubuntu/packages
+sudo chown -R ubuntu:ubuntu /open-aea
+
+pipenv install --skip-lock
+
+pipenv run python /home/ubuntu/watcher.py
