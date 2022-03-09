5a965ef93cfcc930e881da71476085deb3e9c292
8baller <8ball030@gmail.com>
2022-03-09 13:09:47 +0000

    fix: removal of requirements.txt

--- deployments/Dockerfiles/open_aea/start_dev.sh
+++ deployments/Dockerfiles/open_aea/start_dev.sh
@@ -2,6 +2,6 @@ sudo chown -R ubuntu:ubuntu /logs
 sudo chown -R ubuntu:ubuntu /home/ubuntu/packages
 sudo chown -R ubuntu:ubuntu /open-aea
 
-pipenv install --skip-lock
+pipenv install --skip-lock --dev
 
 pipenv run python /home/ubuntu/watcher.py
