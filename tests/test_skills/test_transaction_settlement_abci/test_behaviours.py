d92f8a9a8b91c4e06f65fc19e3e8997c100b6d3b
Adamantios <adamantios.zaras@valory.xyz>
2022-03-17 21:03:53 +0200

    chore: resolve merge conflicts

--- tests/test_skills/test_transaction_settlement_abci/test_behaviours.py
+++ tests/test_skills/test_transaction_settlement_abci/test_behaviours.py
@@ -82,7 +82,9 @@ from tests.test_skills.test_abstract_round_abci.test_common import (
     BaseRandomnessBehaviourTest,
     BaseSelectKeeperBehaviourTest,
 )
-from tests.test_skills.test_price_estimation_abci.test_behaviours import PriceEstimationFSMBehaviourBaseCase
+from tests.test_skills.test_price_estimation_abci.test_behaviours import (
+    PriceEstimationFSMBehaviourBaseCase,
+)
 
 
 class TransactionSettlementFSMBehaviourBaseCase(FSMBehaviourBaseCase):
