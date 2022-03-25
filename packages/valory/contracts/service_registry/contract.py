bd1954431c60d003636c992adc5cdc478528c7ec
m.a.p.karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-03-25 22:26:10 +0100

    feat: first part service registry contract

---
+++ packages/valory/contracts/service_registry/contract.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2022 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This module contains the class to connect to the Service Registry contract."""
+
+import logging
+from typing import List
+
+from aea.configurations.base import PublicId
+from aea.contracts.base import Contract
+from aea_ledger_ethereum import EthereumApi
+
+import functools
+import json
+
+PUBLIC_ID = PublicId.from_str("valory/service_registry:0.1.0")
+
+_logger = logging.getLogger(
+    f"aea.packages.{PUBLIC_ID.author}.contracts.{PUBLIC_ID.name}.contract"
+)
+
+
+class ServiceRegistryContract(Contract):
+    """The Service Registry contract."""
+
+    contract_id = PUBLIC_ID
+
+    @property
+    @functools.lru_cache
+    def json_data(self):
+        """Get contract build json"""
+        json_file_path = "./build/ServiceRegistry.json"
+        with open(json_file_path, "r") as file:
+            data = json.load(file)
+        return data
+
+    @classmethod
+    def get_agent_addresses(cls, ledger_api: EthereumApi, contract_address: str) -> List[str]:
+        """Retrieve on-chain service information"""
+
+        contract_instance = cls.get_instance(ledger_api, contract_address)
+
+        service_info = ledger_api.contract_method_call(
+            contract_instance,
+            "getServiceInfo",
+            # service_id,  # TODO: pass service_id as argument
+        )
+        return service_info[9]
