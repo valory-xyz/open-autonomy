87df8315fe9e95349c8ba555f99e60a7586449e1
Adamantios <adamantios.zaras@valory.xyz>
2022-04-08 18:17:23 +0300

    test: use `HardHatAMMBaseTest` as a base test class
    This fixes the issues with the gnosis safe contract. Previously, the contract was not deployed correctly, which resulted in having no owners, which resulted in transactions getting reverted.
    
    However, `HardHatAMMBaseTest` is an overkill for this test, and it should be a temporary solution.

--- tests/test_skills/test_transaction_settlement_abci/test_failure_modes.py
+++ tests/test_skills/test_transaction_settlement_abci/test_failure_modes.py
@@ -83,7 +83,7 @@ from packages.valory.skills.transaction_settlement_abci.rounds import (
 )
 
 from tests.conftest import ROOT_DIR, make_ledger_api_connection
-from tests.fixture_helpers import HardHatGnosisBaseTest
+from tests.fixture_helpers import HardHatAMMBaseTest
 from tests.helpers.contracts import get_register_contract
 from tests.test_skills.base import FSMBehaviourBaseCase
 
@@ -120,9 +120,7 @@ class OracleBehaviourBaseCase(FSMBehaviourBaseCase):
     price_estimation_period_state: PriceEstimationPeriodState
 
 
-class OracleBehaviourHardHatGnosisBaseCase(
-    OracleBehaviourBaseCase, HardHatGnosisBaseTest
-):
+class OracleBehaviourHardHatGnosisBaseCase(OracleBehaviourBaseCase, HardHatAMMBaseTest):
     """Test tx settlement behaviours in a Hardhat environment, with Gnosis deployed."""
 
     running_loop: asyncio.AbstractEventLoop
@@ -160,7 +158,7 @@ class OracleBehaviourHardHatGnosisBaseCase(
         cls.multiplexer.connect()
 
         # hardhat configuration
-        cls.safe_contract_address = "0xd9Db270c1B5E3Bd161E8c8503c55cEABeE709552"
+        cls.safe_contract_address = "0x68FCdF52066CcE5612827E872c45767E5a1f6551"
         safe_owners = {
             "0xBcd4042DE499D14e55001CcbB24a551F3b954096": "0xf214f2b2cd398c806f84e317254e0f0b801d0643303237d97a22a48e01628897",
             "0x71bE63f3384f5fb98995898A86B02Fb2426c5788": "0x701b615bbdfb9de65240bc28bd21bbc0d996645a3dd57e7b12bc2bdf6f192c82",
