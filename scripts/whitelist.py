ffbb3a44707a2a142bb91a0416df1e8774de514d
Viraj Patel <vptl185@gmail.com>
2022-04-26 18:02:18 +0000

    fix: linters

--- scripts/whitelist.py
+++ scripts/whitelist.py
@@ -3,3 +3,13 @@
 # pylint: skip-file
 click_context  # unused variable (aea_swarm/analyse/__init__.py:30)
 cmd1  # unused function (aea_swarm/analyse/cli.py:25)
+build_deployment  # unused function (aea_swarm/cli/deploy.py:38)
+dev_mode  # unused variable (aea_swarm/cli/deploy.py:67)
+schema_dir  # unused variable (aea_swarm/configurations/base.py:32)
+_validator  # unused attribute (aea_swarm/deploy/base.py:111)
+_validator  # unused attribute (aea_swarm/deploy/base.py:115)
+agent_spec  # unused attribute (aea_swarm/deploy/base.py:330)
+old_wd  # unused variable (aea_swarm/deploy/base.py:402)
+build_docker_compose_yml  # unused function (aea_swarm/deploy/generators/docker_compose/base.py:52)
+HARDHAT_NODE_TEMPLATE  # unused variable (aea_swarm/deploy/generators/docker_compose/templates.py:51)
+HARDHAT_TEMPLATE  # unused variable (aea_swarm/deploy/generators/kubernetes/templates.py:25)
