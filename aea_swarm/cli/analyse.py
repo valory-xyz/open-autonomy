51ff74f35e6e5248d13d4a360f528dd551e30eea
Viraj Patel <vptl185@gmail.com>
2022-04-29 07:04:52 +0000

    feat: port benchmark utils to aea swarm

--- aea_swarm/cli/analyse.py
+++ aea_swarm/cli/analyse.py
@@ -19,11 +19,50 @@
 
 """Analyse CLI module."""
 
+from pathlib import Path
+from typing import Optional
+
 import click
+
+from aea_swarm.analyse.benchmark.aggregate import BlockTypes, aggregate
 
 
 @click.group(name="analyse")
 def analyse_group() -> None:
     """Analyse an AEA project."""
 
-    click.echo("Analyse module.")
+
+@analyse_group.command(name="benchmarks")
+@click.argument(
+    "path",
+    type=click.types.Path(exists=True, dir_okay=True, resolve_path=True),
+    required=True,
+)
+@click.option(
+    "--block-type",
+    "-b",
+    type=click.Choice(choices=(*BlockTypes.types, BlockTypes.ALL), case_sensitive=True),
+    default=BlockTypes.ALL,
+    required=False,
+)
+@click.option(
+    "--period",
+    "-d",
+    type=int,
+    default=-1,
+    required=False,
+)
+@click.option(
+    "--output",
+    "-o",
+    type=click.types.Path(file_okay=True, dir_okay=False, resolve_path=True),
+)
+def benchmark(path: Path, block_type: str, period: int, output: Optional[Path]) -> None:
+    """Benchmark Aggregator."""
+
+    output = Path("./benchmarks.html" if output is None else output).resolve()
+
+    try:
+        aggregate(path=path, block_type=block_type, period=period, output=output)
+    except Exception as e:  # pylint: disable=broad-except
+        raise click.ClickException(str(e)) from e
