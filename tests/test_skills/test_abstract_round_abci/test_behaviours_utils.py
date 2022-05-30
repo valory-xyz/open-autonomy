aa2dede7ff40888c99d886d87a06986603007344
Adamantios <adamantios.zaras@valory.xyz>
2022-05-30 16:45:49 +0300

    test: delete test for removed method

--- tests/test_skills/test_abstract_round_abci/test_behaviours_utils.py
+++ tests/test_skills/test_abstract_round_abci/test_behaviours_utils.py
@@ -1222,38 +1222,6 @@ class TestBaseBehaviour:
         self.behaviour._timeout = timeout
         assert self.behaviour._is_timeout_expired() == expiration_expected
 
-    @mock.patch.object(
-        BaseBehaviour, "_build_http_request_message", return_value=(None, None)
-    )
-    @pytest.mark.parametrize(
-        "response", ({"app_hash": "test"}, {"error": "test"}, None)
-    )
-    def test_get_app_hash(
-        self, _build_http_request_message: mock.Mock, response: Optional[Dict[str, str]]
-    ) -> None:
-        """Test the `_get_app_hash` method."""
-
-        def dummy_do_request(*_: Any) -> Generator[None, None, MagicMock]:
-            """Dummy `_do_request` method."""
-            yield
-            if response is None:
-                return mock.MagicMock(body=b"")
-            return mock.MagicMock(body=json.dumps(response).encode())
-
-        with mock.patch.object(
-            BaseBehaviour, "_do_request", new_callable=lambda *_: dummy_do_request
-        ):
-            app_hash_iter = self.behaviour._get_app_hash()
-            next(app_hash_iter)
-            # perform the last iteration which also returns the result
-            try:
-                next(app_hash_iter)
-            except StopIteration as e:
-                if response is None or response.get("app_hash") is None:
-                    assert e.value is None
-                else:
-                    assert e.value == response["app_hash"]
-
     @mock.patch.object(BaseBehaviour, "_start_reset")
     @mock.patch.object(BaseBehaviour, "_is_timeout_expired")
     def test_reset_tendermint_with_wait_timeout_expired(self, *_: mock.Mock) -> None:
