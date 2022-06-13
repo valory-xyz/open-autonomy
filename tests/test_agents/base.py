4b1e49539db0dc69ccbfd70ed87f7c782bd02b12
Viraj Patel <vptl185@gmail.com>
2022-06-13 08:23:47 +0000

    chore: fix logging issue

--- tests/test_agents/base.py
+++ tests/test_agents/base.py
@@ -122,6 +122,14 @@ class BaseTestEnd2End(AEATestCaseMany, UseFlaskTendermintNode):
         """Set the current agent's config overrides."""
         # each agent has its Tendermint node instance
         self.set_config(
+            "agent.logging_config.handlers.logfile.filename",
+            str(self.t / f"agent_{i}"),
+        )
+        self.set_config(
+            "vendor.valory.connections.abci.config.host",
+            ANY_ADDRESS,
+        )
+        self.set_config(
             "vendor.valory.connections.abci.config.host",
             ANY_ADDRESS,
         )
