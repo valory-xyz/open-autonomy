82ba3a6428c30ced8b853689a152944f6020d9a4
Adamantios <adamantios.zaras@valory.xyz>
2022-06-21 11:26:19 +0300

    chore: remove unnecessary file

--- .setup_oracle.sh
+++
@@ -1,3 +0,0 @@
-swarm deploy build image valory/oracle_hardhat --dev
-swarm deploy build image valory/oracle_hardhat --dependencies
-swarm deploy build deployment valory/oracle_hardhat deployments/keys/hardhat_keys.json --force --dev
