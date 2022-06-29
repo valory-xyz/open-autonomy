e62b4e1e27722338a4434db93b6c07c5946993e8
Viraj Patel <vptl185@gmail.com>
2022-06-29 07:06:56 +0000

    test: added tests for `hash all` command to verify if the agent hashes are updated properly or not

---
+++ tests/test_autonomy/test_cli/test_hash.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2022 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""Test hash command group."""
+
+
+import shutil
+from pathlib import Path
+from typing import Tuple
+
+from aea.helpers.io import from_csv
+
+from autonomy.configurations.loader import load_service_config
+
+from tests.conftest import ROOT_DIR
+from tests.test_autonomy.test_cli.base import BaseCliTest
+
+
+class TestHashAll(BaseCliTest):
+    """Test `hash-all` command."""
+
+    packages_dir: Path
+    cli_options: Tuple[str, ...] = (
+        "hash",
+        "all",
+    )
+
+    @classmethod
+    def setup(cls) -> None:
+        """Setup class."""
+
+        super().setup()
+
+        cls.packages_dir = cls.t / "packages"
+        shutil.copytree(ROOT_DIR / "packages", cls.packages_dir)
+
+    def test_service_hashing(
+        self,
+    ) -> None:
+        """Check if `hash-all` updates agent hashes properly."""
+
+        service_name = "counter"
+        result = self.run_cli(("--packages-dir", str(self.packages_dir)))
+
+        assert result.exit_code == 0
+
+        service_path = self.packages_dir / "valory" / "services" / service_name
+        service_config = load_service_config(service_path)
+
+        hashes_file = self.packages_dir / "hashes.csv"
+        hashes = from_csv(hashes_file)
+
+        assert hashes[f"valory/agents/{service_name}"] == service_config.agent.hash
