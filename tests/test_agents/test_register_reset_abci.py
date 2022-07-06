420849a25d36a1cb9b42ea2fccb9bd6adc2ea4c8
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-07-06 16:52:14 +0200

    tests: pytest markers on register_reset

--- tests/test_agents/test_register_reset_abci.py
+++ tests/test_agents/test_register_reset_abci.py
@@ -18,6 +18,7 @@
 # ------------------------------------------------------------------------------
 
 """Integration tests for the valory/register_reset skill."""
+
 import pytest
 from aea.configurations.data_types import PublicId
 
@@ -31,6 +32,9 @@ HAPPY_PATH = (
 )
 
 
+@pytest.mark.e2e
+@pytest.mark.integration
+@pytest.mark.flaky(reruns=3)
 @pytest.mark.parametrize("nb_nodes", (4,))
 class TestTendermintStartup(BaseTestEnd2EndExecution):
     """Test the ABCI register-reset skill with 4 agents starting up."""
@@ -51,6 +55,9 @@ class TestTendermintStartup(BaseTestEnd2EndExecution):
     ]
 
 
+@pytest.mark.e2e
+@pytest.mark.integration
+@pytest.mark.flaky(reruns=3)
 @pytest.mark.parametrize("nb_nodes", (4,))
 class TestTendermintReset(BaseTestEnd2EndExecution):
     """Test the ABCI register-reset skill with 4 agents when resetting Tendermint."""
@@ -74,6 +81,9 @@ class TestTendermintReset(BaseTestEnd2EndExecution):
     ]
 
 
+@pytest.mark.e2e
+@pytest.mark.integration
+@pytest.mark.flaky(reruns=3)
 @pytest.mark.parametrize("nb_nodes", (4,))
 class TestTendermintResetInterrupt(BaseTestEnd2EndExecution):
     """Test the ABCI register-reset skill with 4 agents when an agent gets interrupted on Tendermint reset."""
@@ -104,6 +114,8 @@ class TestTendermintResetInterrupt(BaseTestEnd2EndExecution):
     n_terminal = 1
 
 
+@pytest.mark.e2e
+@pytest.mark.integration
 class TestTendermintResetInterruptNoRejoin(TestTendermintResetInterrupt):
     """
     Test a Tendermint reset case for the ABCI register-reset skill.
