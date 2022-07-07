ff0c5ffcf1e6ec67a8319027bac50f79ecd4c3a6
Adamantios <adamantios.zaras@valory.xyz>
2022-07-07 16:44:45 +0300

    feat: create a voting payload for a gathering round

--- packages/valory/skills/reset_pause_abci/payloads.py
+++ packages/valory/skills/reset_pause_abci/payloads.py
@@ -19,7 +19,7 @@
 
 """This module contains the transaction payloads for the reset_pause_abci app."""
 from enum import Enum
-from typing import Any, Dict
+from typing import Any, Dict, Optional
 
 from packages.valory.skills.abstract_round_abci.base import BaseTxPayload
 
@@ -27,11 +27,38 @@ from packages.valory.skills.abstract_round_abci.base import BaseTxPayload
 class TransactionType(Enum):
     """Enumeration of transaction types."""
 
+    GATHER = "gather"
     RESETPAUSE = "reset_pause"
 
     def __str__(self) -> str:
         """Get the string value of the transaction type."""
         return self.value
+
+
+class GatherPayload(BaseTxPayload):
+    """Represent a transaction payload of type 'gather'."""
+
+    transaction_type = TransactionType.GATHER
+
+    def __init__(self, sender: str, vote: Optional[bool] = None, **kwargs: Any) -> None:
+        """Initialize a 'gather' transaction payload.
+
+        :param sender: the sender (Ethereum) address
+        :param vote: the vote
+        :param kwargs: the keyword arguments
+        """
+        super().__init__(sender, **kwargs)
+        self._vote = vote
+
+    @property
+    def vote(self) -> Optional[bool]:
+        """Get the vote."""
+        return self._vote
+
+    @property
+    def data(self) -> Dict:
+        """Get the data."""
+        return dict(vote=self.vote) if self.vote is not None else {}
 
 
 class ResetPausePayload(BaseTxPayload):
