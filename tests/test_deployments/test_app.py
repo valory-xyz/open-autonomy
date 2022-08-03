f2561a0ad90596956908e8a7ec4d31cc16801903
David Vilela <dvilelaf@gmail.com>
2022-08-03 11:35:33 +0200

    fix: kill node on test teardown

--- tests/test_deployments/test_app.py
+++ tests/test_deployments/test_app.py
@@ -336,6 +336,8 @@ class TestTendermintBufferFailing(BaseTendermintServerTest):
     @classmethod
     def teardown_class(cls) -> None:
         """Teardown the test."""
+        # After the test, the node has hanged. We need to kill it and not stop it.
+        cls.tendermint_node._process.kill()
         cls.app_context.pop()
         shutil.rmtree(cls.tm_home, ignore_errors=True, onerror=readonly_handler)
 
