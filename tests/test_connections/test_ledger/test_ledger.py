1d05b35348afcdd4c7bf48dfd58bfa0fb0bb3550
Adamantios <adamantios.zaras@valory.xyz>
2022-08-18 18:16:08 +0300

    test: create a test for hanging using the multiplexer

--- tests/test_connections/test_ledger/test_ledger.py
+++ tests/test_connections/test_ledger/test_ledger.py
@@ -20,22 +20,34 @@
 """This module contains the tests of the ledger connection module."""
 
 import asyncio
+import logging
 from asyncio import Task
-from typing import Any
+from pathlib import Path
+from threading import Thread
+from typing import Any, Callable, cast
 from unittest import mock
 
 import pytest
 from aea.configurations.base import ConnectionConfig
+from aea.mail.base import Envelope
+from aea.multiplexer import Multiplexer
+from aea.test_tools.test_skill import BaseSkillTestCase
 
+from packages.valory.connections.abci.dialogues import AbciDialogues
 from packages.valory.connections.ledger.connection import LedgerConnection
+from packages.valory.protocols.abci import AbciMessage
+from packages.valory.skills.abstract_abci.handlers import ABCIHandler
+
+from tests.conftest import ROOT_DIR, make_ledger_api_connection
 
 
-def dummy_task_wrapper(waiting_time: float, result: Any) -> Task:
-    """Create a dummy task that simply waits for a given `waiting_time` and returns a given `result`."""
+def dummy_task_wrapper(waiting_time: float, result_message: AbciMessage) -> Task:
+    """Create a dummy task that simply waits for a given `waiting_time` and returns a given `result_message`."""
 
-    async def dummy_task(*_: Any) -> None:
-        """Wait for the given `waiting_time` and return the given `result`."""
-        await asyncio.sleep(waiting_time, result)
+    async def dummy_task(*_: Any) -> AbciMessage:
+        """Wait for the given `waiting_time` and return the given `result_message`."""
+        await asyncio.sleep(waiting_time)
+        return result_message
 
     task = asyncio.create_task(dummy_task())
     return task
@@ -69,7 +81,12 @@ class TestLedgerConnection:
         receive_task = asyncio.ensure_future(ledger_connection.receive())
 
         # create a blocking task lasting `blocking_time` secs
-        blocking_task = dummy_task_wrapper(blocking_time, "blocking_task")
+        blocking_task = dummy_task_wrapper(
+            blocking_time,
+            AbciMessage(
+                AbciMessage.Performative.DUMMY, _body={"data": b"blocking_task"}  # type: ignore
+            ),
+        )
         dummy_envelope = mock.MagicMock()
         with mock.patch.object(
             LedgerConnection, "_schedule_request", return_value=blocking_task
@@ -78,7 +95,10 @@ class TestLedgerConnection:
 
         # create a non-blocking task lasting `non_blocking_time` secs, after `wait_time_among_tasks`
         await asyncio.sleep(wait_time_among_tasks)
-        normal_task = dummy_task_wrapper(non_blocking_time, "normal_task")
+        normal_task = dummy_task_wrapper(
+            non_blocking_time,
+            AbciMessage(AbciMessage.Performative.DUMMY, _body={"data": b"normal_task"}),  # type: ignore
+        )
         dummy_envelope = mock.MagicMock()
         with mock.patch.object(
             LedgerConnection, "_schedule_request", return_value=normal_task
@@ -96,3 +116,112 @@ class TestLedgerConnection:
 
         # the `receive_task` should be done, and not await for the `blocking_task`
         assert receive_task.done(), "Receive task is blocked by blocking task!"
+
+
+class TestLedgerConnectionWithMultiplexer(BaseSkillTestCase):
+    """Test `LedgerConnection` class, using the multiplexer, via the `BaseSkillTestCase`."""
+
+    path_to_skill = Path(ROOT_DIR, "packages", "valory", "skills", "abstract_abci")
+    abci_handler: ABCIHandler
+    logger: logging.Logger
+    abci_dialogues: AbciDialogues
+    running_loop: asyncio.AbstractEventLoop
+    thread_loop: Thread
+    _multiplexer: Multiplexer
+    ledger_connection: LedgerConnection
+    make_ledger_connection_callable: Callable = make_ledger_api_connection
+
+    @classmethod
+    def setup(cls, **kwargs: Any) -> None:
+        """Setup the test class."""
+        super().setup()
+        cls.abci_handler = cast(ABCIHandler, cls._skill.skill_context.handlers.abci)
+        cls.logger = cls._skill.skill_context.logger
+
+        cls.abci_dialogues = cast(
+            AbciDialogues, cls._skill.skill_context.abci_dialogues
+        )
+
+        # set up a multiplexer with the required ledger connection
+        cls.running_loop = asyncio.new_event_loop()
+        cls.thread_loop = Thread(target=cls.running_loop.run_forever)
+        cls.thread_loop.start()
+        cls._multiplexer = Multiplexer(
+            [cls.make_ledger_connection_callable()], loop=cls.running_loop
+        )
+        cls._multiplexer.connect()
+        # the ledger connection's connect() is called by the multiplexer
+        # once a connection is ready, `receive()` is called by the multiplexer
+        cls.ledger_connection = cast(LedgerConnection, cls._multiplexer.connections[0])
+
+    @classmethod
+    def teardown(cls) -> None:
+        """Tear down the multiplexer."""
+        cls._multiplexer.disconnect()
+        cls.running_loop.call_soon_threadsafe(cls.running_loop.stop)
+        cls.thread_loop.join()
+
+    @pytest.mark.asyncio
+    async def test_not_hanging_with_multiplexer(self) -> None:
+        """Test that the connection does not hang and that the tasks cannot get blocked, using the multiplexer."""
+        # create configurations for the test, re bocking and non-blocking tasks' waiting times
+        blocking_time = 100
+        wait_time_among_tasks = 0.1
+        non_blocking_time = 1
+        tolerance = 1
+        assert (
+            non_blocking_time + tolerance + wait_time_among_tasks < blocking_time
+        ), "`non_blocking_time + tolerance + wait_time_among_tasks` should be less than the `blocking_time`."
+
+        # create a blocking task lasting `blocking_time` secs
+        blocking_task = dummy_task_wrapper(
+            blocking_time,
+            AbciMessage(
+                AbciMessage.Performative.DUMMY, _body={"data": b"blocking_task"}  # type: ignore
+            ),
+        )
+        blocking_dummy_envelope = Envelope(
+            to="test_blocking_to",
+            sender="test_blocking_sender",
+            message=AbciMessage(AbciMessage.Performative.DUMMY),  # type: ignore
+        )
+        with mock.patch.object(
+            self.ledger_connection, "_schedule_request", return_value=blocking_task
+        ):
+            await self.ledger_connection.send(blocking_dummy_envelope)
+
+        # create a non-blocking task lasting `non_blocking_time` secs, after `wait_time_among_tasks`
+        await asyncio.sleep(wait_time_among_tasks)
+        normal_task = dummy_task_wrapper(
+            non_blocking_time,
+            AbciMessage(AbciMessage.Performative.DUMMY, _body={"data": b"normal_task"}),  # type: ignore
+        )
+        normal_dummy_envelope = Envelope(
+            to="test_normal_to",
+            sender="test_normal_sender",
+            message=AbciMessage(AbciMessage.Performative.DUMMY),  # type: ignore
+        )
+        with mock.patch.object(
+            self.ledger_connection, "_schedule_request", return_value=normal_task
+        ):
+            await self.ledger_connection.send(normal_dummy_envelope)
+
+        # the response envelopes of the ledger connection should be empty
+        assert self.ledger_connection.response_envelopes.empty()
+        # `receive()` should not be done,
+        # and multiplexer's `_receiving_loop` should be still running and have an empty `in_queue`
+        assert (
+            len(self._multiplexer.in_queue.queue) == 0
+        ), "The multiplexer's `in_queue` should not contain anything."
+
+        # sleep for `non_blocking_time + tolerance`
+        await asyncio.sleep(non_blocking_time + tolerance)
+
+        # the normal task should be finished
+        assert normal_task.done(), "Normal task should be done at this point."
+
+        # the blocking task should not be done
+        assert not blocking_task.done(), "Blocking task should be still running."
+
+        # the response envelopes of the ledger connection should now contain the `normal_dummy_envelope`
+        assert not self.ledger_connection.response_envelopes.empty()
