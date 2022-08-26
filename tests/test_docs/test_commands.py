5a9ce4c72fb14980e8d0d51362da6564182e89dd
David Vilela <dvilelaf@gmail.com>
2022-08-26 18:19:47 +0200

    fix: refactor, linters

--- tests/test_docs/test_commands.py
+++ tests/test_docs/test_commands.py
@@ -21,40 +21,48 @@
 
 import itertools
 import re
+from pathlib import Path
+from typing import Dict, Union
 
 import click
-from click.core import Option
+from click.core import Command, Group, Option
 
 from autonomy.cli import cli as autonomy_cli
+from scripts.check_doc_ipfs_hashes import AEA_COMMAND_REGEX, read_file
+
+
+def get_cmd_data(cmd: Union[Command, Group]) -> Dict:
+    """Returns a dict containing the command options and arguments."""
+
+    return {
+        "commands": {},
+        "options": list(
+            itertools.chain(*[i.opts for i in cmd.params if isinstance(i, Option)])
+        ),
+        "arguments": list(
+            itertools.chain(
+                *[i.opts for i in cmd.params if isinstance(i, click.Argument)]
+            )
+        ),
+    }
 
 
 class CommandValidator:
     """Validates commands against a CLI"""
 
-    def __init__(self, cli):
+    def __init__(self, cli: Group):
         """Extract autonomy command tree from the autonomy cli"""
+
+        # CLI level
         self.tree = {
-            "commands": {
-                cli.name: {
-                    "commands": {},
-                    "options": list(
-                        itertools.chain(
-                            *[i.opts for i in cli.params if isinstance(i, Option)]
-                        )
-                    ),
-                }
-            },
+            "commands": {cli.name: get_cmd_data(cli)},
         }
 
+        # Command level
         for cmd_name, cmd in cli.commands.items():
-            self.tree["commands"][cli.name]["commands"][cmd_name] = {
-                "options": list(
-                    itertools.chain(
-                        *[i.opts for i in cmd.params if isinstance(i, Option)]
-                    )
-                ),
-                "commands": {},
-            }
+            self.tree["commands"][cli.name]["commands"][cmd_name] = get_cmd_data(cmd)
+
+            # Sub-command level
             if isinstance(cmd, click.Group):
                 for sub_cmd_name in cmd.list_commands(click.Context):
                     sub_cmd = cli.commands[cmd_name].get_command(
@@ -62,36 +70,87 @@ class CommandValidator:
                     )
                     self.tree["commands"][cli.name]["commands"][cmd_name]["commands"][
                         sub_cmd_name
-                    ] = {
-                        "options": list(
-                            itertools.chain(
-                                *[
-                                    i.opts
-                                    for i in sub_cmd.params
-                                    if isinstance(i, Option)
-                                ]
-                            )
-                        )
-                    }
+                    ] = get_cmd_data(sub_cmd)
 
     def validate(self, cmd: str) -> bool:
+        """Validates a command"""
+
+        # Copy the tree
         tree = self.tree
+
+        # Iterate the command parts
         for cmd_part in cmd.split(" "):
-            if cmd_part not in tree["commands"].keys():
-                print(f"Error in {cmd_part}")
+
+            # Check options
+            if cmd_part.startswith("-") and cmd_part not in tree["options"]:
+                print(
+                    f"Command validation error: option '{cmd_part}' is not present on the command tree {list(tree['options'])}:\n    {cmd}"
+                )
                 return False
-            print(list(tree["commands"].keys()))
-            tree = tree["commands"][cmd_part]
+
+            # Leaf: check arguments
+            if not tree["commands"].keys():
+                if len(tree["arguments"]) == 0:
+                    print(
+                        f"Command validation error: argument '{cmd_part}' is not present on the command tree {list(tree['arguments'].keys())}:\n    {cmd}"
+                    )
+                    return False
+
+                # Stop if this is the final command part
+                if cmd.endswith(cmd_part):
+                    break
+                continue
+
+            # Branch: check command
+            if cmd_part not in tree["commands"].keys():
+                print(
+                    f"Command validation error: command '{cmd_part}' is not present on the command tree {list(tree['commands'].keys())}:\n    {cmd}"
+                )
+                return False
+
+            # Update the tree if this is not an option or an argument.
+            # Options/arguments are at the same level as their corresponding commands.
+            if not cmd_part.startswith("-") and tree["commands"].keys():
+                tree = tree["commands"][cmd_part]
 
         return True
 
 
-cmd = "autonomy deploy build keys.json"
-v = CommandValidator(autonomy_cli)
-print(v.validate(cmd))
+def test_validate_doc_commands() -> None:
+    """Test that doc commands are valid"""
 
-# cmd = autonomy_cli.commands["deploy"].get_command(click.Context, "build")
+    # Get the markdown files and the Makefile
+    target_files = list(Path("docs").rglob("*.md"))
+    target_files.append(Path("Makefile"))
 
-# print(dir(cmd))
+    # Get the validator
+    validator = CommandValidator(autonomy_cli)
 
-# print([i.opts for i in cmd.params if isinstance(i, click.Argument)])
+    #
+    for file_ in target_files:
+        content = read_file(str(file_))
+
+        # The regex currently finds package related commands. We need to use a general one.
+        for match in [m.groupdict() for m in re.finditer(AEA_COMMAND_REGEX, content)]:
+            assert validator.validate(match["full_cmd"])
+
+
+def test_validator() -> None:
+    """Test the command validator"""
+
+    validator = CommandValidator(autonomy_cli)
+
+    good_cmds = [
+        "autonomy deploy build keys.json",
+        "autonomy deploy build --docker --remote keys.json",
+    ]
+
+    bad_cmds = [
+        "autonomy deploy build --docker --remoote keys.json",
+    ]
+
+    for cmd in good_cmds:
+        assert validator.validate(cmd), f"Command {cmd} is not valid"
+
+    for cmd in bad_cmds:
+        assert not validator.validate(cmd), f"Command {cmd} is valid and it shouldn't."
