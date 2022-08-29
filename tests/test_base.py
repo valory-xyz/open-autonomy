b31ea6f9d93cc322ae33366e628e8c01927cc92b
Viraj Patel <vptl185@gmail.com>
2022-08-29 12:34:28 +0000

    chore: bump open-autonomy for v0.2.1.post1

--- tests/test_base.py
+++ tests/test_base.py
@@ -47,7 +47,7 @@ def get_test_files(package_type: PackageType) -> List[Path]:
 
 def test_version() -> None:
     """Test the version."""
-    assert autonomy.__version__ == "0.2.1"
+    assert autonomy.__version__ == "0.2.1.post1"
 
 
 @pytest.mark.parametrize(
