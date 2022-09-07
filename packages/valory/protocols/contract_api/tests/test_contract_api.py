9edd49eebde838085609d970374ec4f381172373
Viraj Patel <vptl185@gmail.com>
2022-09-07 07:48:51 +0000

    feat: port contract api protocol test to the package

--- packages/valory/protocols/contract_api/tests/test_contract_api.py
+++ packages/valory/protocols/contract_api/tests/test_contract_api.py
@@ -30,8 +30,7 @@ from aea.protocols.base import Message
 from aea.protocols.dialogue.base import Dialogue as BaseDialogue
 from aea.protocols.dialogue.base import DialogueLabel
 
-import packages
-from packages.valory.protocols.contract_api import ContractApiMessage
+from packages.valory.protocols.contract_api import ContractApiMessage, message
 from packages.valory.protocols.contract_api.custom_types import Kwargs
 from packages.valory.protocols.contract_api.dialogues import (
     ContractApiDialogue,
@@ -200,7 +199,7 @@ class TestState(BaseTestMessageConstruction):
 
 
 @mock.patch.object(
-    packages.valory.protocols.contract_api.message,
+    message,
     "enforce",
     side_effect=AEAEnforceError("some error"),
 )
