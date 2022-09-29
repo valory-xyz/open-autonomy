ba832d71104875a26ac84e9a2e1a3ace05436cd8
Viraj Patel <vptl185@gmail.com>
2022-09-29 06:41:07 +0000

    fix: add flags with dummy path values

--- tests/test_autonomy/test_cli/test_deploy/test_build/test_deployment.py
+++ tests/test_autonomy/test_cli/test_deploy/test_build/test_deployment.py
@@ -180,7 +180,11 @@ class TestBuildDeployment(BaseCliTest):
                     "--dev",
                     "--local",
                     "--packages-dir",
-                    str(ROOT_DIR / PACKAGES),
+                    str(ROOT_DIR),
+                    "--open-aea-dir",
+                    str(ROOT_DIR),
+                    "--open-autonomy-dir",
+                    str(ROOT_DIR),
                 )
             )
 
@@ -294,7 +298,11 @@ class TestBuildDeployment(BaseCliTest):
                     "--dev",
                     "--local",
                     "--packages-dir",
-                    str(ROOT_DIR / PACKAGES),
+                    str(ROOT_DIR),
+                    "--open-aea-dir",
+                    str(ROOT_DIR),
+                    "--open-autonomy-dir",
+                    str(ROOT_DIR),
                 )
             )
 
