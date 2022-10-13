72f13125b292dffcd2fb64577dd06a0558817a9a
David Vilela <dvilelaf@gmail.com>
2022-10-13 12:04:04 +0200

    fix: parallelize link check

--- scripts/check_doc_links.py
+++ scripts/check_doc_links.py
@@ -23,8 +23,9 @@
 
 import re
 import sys
+from concurrent.futures import ThreadPoolExecutor
 from pathlib import Path
-from typing import Any, List, Tuple
+from typing import Any, Dict, List, Tuple
 
 import requests
 from requests.adapters import HTTPAdapter  # type: ignore
@@ -33,6 +34,7 @@ from requests.packages.urllib3.util.retry import (  # type: ignore # pylint: dis
 )
 
 
+MAX_WORKERS = 10
 URL_REGEX = r'(https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s)"]{2,}|www\.[a-zA-Z0-9][a-zA-Z0-9-]+[a-zA-Z0-9]\.[^\s)"]{2,}|https?:\/\/(?:www\.|(?!www))[a-zA-Z0-9]+\.[^\s)"]{2,}|www\.[a-zA-Z0-9]+\.[^\s)"]{2,})'
 
 
@@ -43,12 +45,49 @@ def read_file(filepath: str) -> str:
     return file_str
 
 
+def check_file(
+    session: Any, md_file: str, http_skips: List[str], url_skips: List[str]
+) -> Tuple:
+    """Check for broken or HTTP links in a specific file"""
+
+    text = read_file(md_file)
+    m = re.findall(URL_REGEX, text)
+    http_links = []
+    broken_links = []
+
+    for url in m:
+
+        # Add the closing parenthesis if it is missing, as the REGEX is too strict sometimes
+        if "(" in url and ")" not in url:
+            url += ")"
+
+        # Check for HTTP urls
+        if not url.startswith("https") and url not in http_skips:
+            http_links.append((md_file, url))
+
+        # Check for broken links: 200 and 403 codes are admitted
+        if url in url_skips + http_skips:
+            continue
+        try:
+            status_code = session.get(url, timeout=5).status_code
+            if status_code not in (200, 403):
+                broken_links.append((md_file, url, status_code))
+        except (
+            requests.exceptions.RetryError,
+            requests.exceptions.ConnectionError,
+        ) as e:
+            broken_links.append((md_file, url, e))
+
+    return str(md_file), http_links, broken_links
+
+
 def main() -> None:  # pylint: disable=too-many-locals
     """Check for broken or HTTP links"""
     all_md_files = [str(p.relative_to(".")) for p in Path("docs").rglob("*.md")]
 
-    broken_links: List[Tuple[str, Any, Any]] = []
-    http_links = []
+    broken_links: Dict[str, List[str]] = {}
+    http_links: Dict[str, List[str]] = {}
+
     http_skips = [
         "http://www.fipa.org/repository/ips.php3",
         "http://host.docker.internal:8545```",
@@ -71,53 +110,52 @@ def main() -> None:  # pylint: disable=too-many-locals
     session.mount("https://", adapter)
     session.mount("http://", adapter)
 
-    for md_file in all_md_files:
-        print(f"Checking {md_file}...", end="")
-        text = read_file(md_file)
-        m = re.findall(URL_REGEX, text)
-        error = False
-        for url in m:
-            # Add the closing parenthesis if it is msising, as the REGEX is too strict sometimes
-            if "(" in url and ")" not in url:
-                url += ")"
+    # Run all file checks in a thread pool
+    with ThreadPoolExecutor(max_workers=MAX_WORKERS) as executor:
+        futures = []
+        for md_file in all_md_files:
+            print(f"Checking {str(md_file)}...")
+            futures.append(
+                executor.submit(check_file, session, md_file, http_skips, url_skips)
+            )
 
-            # Check for HTTP urls
-            if not url.startswith("https") and url not in http_skips:
-                http_links.append((md_file, url))
-                error = True
+        # Awaiting for results is blocking
+        print("Awaiting for results...")
+        future_results = [future.result() for future in futures]
 
-            # Check for broken links: 200 and 403 codes are admitted
-            if url in url_skips + http_skips:
-                continue
-            try:
-                status_code = session.get(url, timeout=5).status_code
-                if status_code not in (200, 403):
-                    broken_links.append((md_file, url, status_code))
-                    error = True
-            except (
-                requests.exceptions.RetryError,
-                requests.exceptions.ConnectionError,
-            ) as e:
-                broken_links.append((md_file, url, e))
-                error = True
-        print("ERROR" if error else "OK")
+        # Get errors
+        for i in future_results:
+            if i[1]:
+                http_links[i[0]] = i[1]
+            if i[2]:
+                broken_links[i[0]] = i[2]
 
-    broken_links_str = "\n".join(
-        [f"{url[0]}: {url[1]} ({url[2]})" for url in broken_links]
-    )
-    http_links_str = "\n".join([f"{url[0]}: {url[1]}" for url in http_links])
+        # Check errors
+        if broken_links:
+            broken_links_str = "\n".join(
+                [
+                    f"{file_name}: {[url[1] for url in urls]}"
+                    for file_name, urls in broken_links.items()
+                ]
+            )
+            print(f"Found broken url in the docs:\n{broken_links_str}")
 
-    if broken_links:
-        print(f"Found broken url in the docs:\n{broken_links_str}")
+        if http_links:
+            http_links_str = "\n".join(
+                [
+                    f"{file_name}: {[url[1] for url in urls]}"
+                    for file_name, urls in http_links.items()
+                ]
+            )
+            print(
+                f"Found HTTP urls in the docs:\n{http_links_str}\nTry to use HTTPS equivalent urls or add them to 'http_skips' if not possible"
+            )
 
-    if http_links:
-        print(
-            f"Found HTTP urls in the docs:\n{http_links_str}\nTry to use HTTPS equivalent urls or add them to 'http_skips' if not possible"
-        )
+        if broken_links or http_links:
+            sys.exit(1)
 
-    if broken_links or http_links:
-        sys.exit(1)
-    sys.exit(0)
+        print("OK")
+        sys.exit(0)
 
 
 if __name__ == "__main__":
