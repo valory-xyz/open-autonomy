76ce3684778f78a4dbe06827afed8d3cf375bd3a
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-11-01 22:27:44 +0100

    feat: data/dummy_skill

---
+++ packages/valory/skills/abstract_round_abci/tests/data/dummy_abci/behaviours.py
@@ -0,0 +1,144 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2022 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This package contains round behaviours of DummyAbciApp."""
+
+from typing import Generator, Set, Type, cast
+
+from packages.valory.skills.abstract_round_abci.base import AbstractRound
+from packages.valory.skills.abstract_round_abci.behaviours import (
+    AbstractRoundBehaviour,
+    BaseBehaviour,
+)
+from packages.valory.skills.abstract_round_abci.common import RandomnessBehaviour
+from packages.valory.skills.abstract_round_abci.tests.data.dummy_abci.models import (
+    Params,
+)
+from packages.valory.skills.abstract_round_abci.tests.data.dummy_abci.payloads import (
+    DummyFinalPayload,
+    DummyKeeperSelectionPayload,
+    DummyRandomnessPayload,
+    DummyStartingPayload,
+)
+from packages.valory.skills.abstract_round_abci.tests.data.dummy_abci.rounds import (
+    DummyAbciApp,
+    DummyFinalRound,
+    DummyKeeperSelectionRound,
+    DummyRandomnessRound,
+    DummyStartingRound,
+    SynchronizedData,
+)
+
+
+class DummyBaseBehaviour(BaseBehaviour):
+    """Base behaviour for the common apps' skill."""
+
+    @property
+    def synchronized_data(self) -> SynchronizedData:
+        """Return the synchronized data."""
+        return cast(SynchronizedData, super().synchronized_data)
+
+    @property
+    def params(self) -> Params:
+        """Return the params."""
+        return cast(Params, super().params)
+
+
+class DummyStartingBehaviour(DummyBaseBehaviour):
+    """DummyStartingBehaviour"""
+
+    behaviour_id: str = "dummy_starting"
+    matching_round: Type[AbstractRound] = DummyStartingRound
+
+    def async_act(self) -> Generator:
+        """Do the act, supporting asynchronous execution."""
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).local():
+            content = True
+            sender = self.context.agent_address
+            payload = DummyStartingPayload(sender=sender, content=content)
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).consensus():
+            yield from self.send_a2a_transaction(payload)
+            yield from self.wait_until_round_end()
+
+        self.set_done()
+
+
+class DummyRandomnessBehaviour(RandomnessBehaviour):
+    """DummyRandomnessBehaviour"""
+
+    behaviour_id: str = "dummy_randomness"
+    matching_round: Type[AbstractRound] = DummyRandomnessRound
+    payload_class = DummyRandomnessPayload
+
+
+class DummyKeeperSelectionBehaviour(DummyBaseBehaviour):
+    """DummyKeeperSelectionBehaviour"""
+
+    behaviour_id: str = "dummy_keeper_selection"
+    matching_round: Type[AbstractRound] = DummyKeeperSelectionRound
+
+    def async_act(self) -> Generator:
+        """Do the act, supporting asynchronous execution."""
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).local():
+            content = True
+            sender = self.context.agent_address
+            payload = DummyKeeperSelectionPayload(sender=sender, content=content)
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).consensus():
+            yield from self.send_a2a_transaction(payload)
+            yield from self.wait_until_round_end()
+
+        self.set_done()
+
+
+class DummyFinalBehaviour(DummyBaseBehaviour):
+    """DummyFinalBehaviour"""
+
+    behaviour_id: str = "dummy_final"
+    matching_round: Type[AbstractRound] = DummyFinalRound
+
+    def async_act(self) -> Generator:
+        """Do the act, supporting asynchronous execution."""
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).local():
+            content = True
+            sender = self.context.agent_address
+            payload = DummyFinalPayload(sender=sender, content=content)
+
+        with self.context.benchmark_tool.measure(self.behaviour_id).consensus():
+            yield from self.send_a2a_transaction(payload)
+            yield from self.wait_until_round_end()
+
+        self.set_done()
+
+
+class DummyRoundBehaviour(AbstractRoundBehaviour):
+    """DummyRoundBehaviour"""
+
+    initial_behaviour_cls = DummyStartingBehaviour
+    abci_app_cls = DummyAbciApp  # type: ignore
+    behaviours: Set[Type[BaseBehaviour]] = [
+        DummyFinalBehaviour,
+        DummyKeeperSelectionBehaviour,
+        DummyRandomnessBehaviour,
+        DummyStartingBehaviour,
+    ]
