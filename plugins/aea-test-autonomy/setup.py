e7ba83f0dc6d112c8459ff095edea6c7b232120d
Viraj Patel <vptl185@gmail.com>
2022-11-09 13:13:47 +0530

    chore: bump open-aea version to v1.23.0

--- plugins/aea-test-autonomy/setup.py
+++ plugins/aea-test-autonomy/setup.py
@@ -24,7 +24,7 @@ from setuptools import setup  # type: ignore
 
 
 base_deps = [
-    "open-aea[all]>=1.22.0.post1,<2.0.0",
+    "open-aea[all]>=1.23.0,<2.0.0",
     "pytest==7.0.0",
     "open-aea-ledger-ethereum==1.22.0",
     "docker==6.0.0",
