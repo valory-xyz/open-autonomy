21a8f98eb63fc7d2db6dea14831cfb701b2b97e4
Ardian <ardianabazi09@gmail.com>
2022-11-15 19:26:16 +0100

    fix: add `initContainers` to cp tm config in k8s deployment [no-ci]

--- autonomy/deploy/generators/kubernetes/templates.py
+++ autonomy/deploy/generators/kubernetes/templates.py
@@ -218,7 +218,7 @@ spec:
           - name: PROXY_APP
             value: tcp://localhost:26658
           - name: TMHOME
-            value: /tendermint/node{validator_ix}
+            value: /tm
           - name: CREATE_EMPTY_BLOCKS
             value: "true"
           - name: LOG_FILE
@@ -232,7 +232,7 @@ spec:
           - mountPath: /logs
             name: persistent-data
           - mountPath: /tendermint
-            name: nodes
+            name: local-tendermint
 
       - name: aea
         image: {runtime_image}
@@ -281,6 +281,17 @@ spec:
         - name: nodes
           persistentVolumeClaim:
             claimName: 'nodes'
+        - emptyDir: {}
+          name: local-tm
+      initContainers:
+        - name: copy-tendermint-configuration
+          image: "ubuntu/ubuntu:20.04"
+          command: ["cp", "-r", "/tendermint/node{validator_ix}", "/tm"]
+          volumeMounts:
+            - name: nodes
+              mountPath: /tendermint
+            - name: local-tendermint
+              mountPath: /tm
 """
 
 AGENT_SECRET_TEMPLATE: str = """
