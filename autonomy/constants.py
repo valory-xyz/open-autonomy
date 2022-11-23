0443a716c41c3102c7777d0be68d55d99831b86b
Adamantios <adamantios.zaras@valory.xyz>
2022-11-23 17:18:56 +0200

    fix: add missing dependency

--- autonomy/constants.py
+++ autonomy/constants.py
@@ -57,4 +57,4 @@ DEFAULT_SERVICE_REGISTRY_CONTRACTS_IMAGE = (
 ACN_IMAGE_NAME = os.environ.get("ACN_IMAGE_NAME", "valory/open-acn-node")
 
 OAR_IMAGE = "valory/oar-{agent}:{version}"
-ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeifwncqpgye7l5o3wrzzrfsetm34tabun45kslljhpkdfursr6umkq"
+ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeian3uuk4t6n3f45czwtwisnvn7abzvjkypu7jhmymuuoou5qlfu3u"
--- docs/demos/simple_abci.md
+++ docs/demos/simple_abci.md
@@ -35,7 +35,7 @@ you have followed the [setup instructions](../guides/set_up.md#setup). As a resu
 
 2. Use the CLI to fetch the `valory/simple_abci` service.
     ```bash
-    autonomy fetch valory/simple_abci:0.1.0:bafybeibcnorpd5nb7phens67vobosyfllils2gffiumnhp3efzpuhhh2gi --remote --service
+    autonomy fetch valory/simple_abci:0.1.0:bafybeiacegiksoypnuqbpmckqlikl3snvzddryitpllkgwqzt723xxi3oa --remote --service
     cd simple_abci
     ```
 
