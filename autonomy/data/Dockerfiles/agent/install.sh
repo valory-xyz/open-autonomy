83ae77c800ae0f62f491d2ca36e0d46123328ce6
Viraj Patel <vptl185@gmail.com>
2022-12-02 19:11:46 +0530

    fix: exit on build failure when building agent images

--- autonomy/data/Dockerfiles/agent/install.sh
+++ autonomy/data/Dockerfiles/agent/install.sh
@@ -9,9 +9,9 @@ fi
 echo Running the aea with $(aea --version)
 
 echo "Loading $AEA_AGENT"
-aea fetch $AEA_AGENT --alias agent
+aea fetch $AEA_AGENT --alias agent || (echo "Fetching agent failed" && exit 1)
 cd agent
 
 echo "Installing the necessary dependencies!"
-aea install
+aea install || (echo "Agent dependency installation failed" && exit 1)
 cd ..
