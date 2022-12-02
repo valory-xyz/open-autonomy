d6ac8b182965654eb8aee7d875ab50aa3d78cef3
Ardian <ardianabazi09@gmail.com>
2022-12-02 14:35:11 +0100

    chore: add sleep_after_hard_reset and sleep_before_hard_reset props

--- packages/valory/skills/abstract_round_abci/behaviour_utils.py
+++ packages/valory/skills/abstract_round_abci/behaviour_utils.py
@@ -1607,6 +1607,28 @@ class BaseBehaviour(AsyncBehaviour, IPFSBehaviour, CleanUpBehaviour, ABC):
             return RPCResponseStatus.ALREADY_KNOWN
         return RPCResponseStatus.UNCLASSIFIED_ERROR
 
+    @property
+    def sleep_before_hard_reset(self) -> float:
+        """
+        Amount of time to sleep before performing a hard reset.
+
+        We sleep for half the observation interval as there are no immediate transactions on either side of the reset.
+
+        :returns: the amount of time to sleep in seconds
+        """
+        return self.params.observation_interval / 2
+
+    @property
+    def sleep_after_hard_reset(self) -> float:
+        """
+        Amount of time to sleep after performing a hard reset.
+
+        We sleep for half the observation interval as there are no immediate transactions on either side of the reset.
+
+        :returns: the amount of time to sleep in seconds
+        """
+        return self.params.observation_interval / 2
+
     def _start_reset(self) -> Generator:
         """Start tendermint reset.
 
@@ -1616,10 +1638,7 @@ class BaseBehaviour(AsyncBehaviour, IPFSBehaviour, CleanUpBehaviour, ABC):
         :yield: None
         """
         if self._check_started is None and not self._is_healthy:
-            # we do the reset in the middle of the pause as there are no immediate transactions on either side of the reset
-            yield from self.wait_from_last_timestamp(
-                self.params.observation_interval / 2
-            )
+            yield from self.wait_from_last_timestamp(self.sleep_before_hard_reset)
             self._check_started = datetime.datetime.now()
             self._timeout = self.params.max_healthcheck
             self._is_healthy = False
@@ -1723,7 +1742,10 @@ class BaseBehaviour(AsyncBehaviour, IPFSBehaviour, CleanUpBehaviour, ABC):
                     for handler_name in self.context.handlers.__dict__.keys():
                         dialogues = getattr(self.context, f"{handler_name}_dialogues")
                         dialogues.cleanup()
-
+                    # in case of successful reset we store the reset params in the shared state,
+                    # so that in the future if the communication with tendermint breaks, and we need to
+                    # perform a hard reset to restore it, we can use these as the right ones
+                    self.context.shared_state["reset_params"] = reset_params
                     self._end_reset()
                 else:
                     msg = response.get("message")
@@ -1762,7 +1784,7 @@ class BaseBehaviour(AsyncBehaviour, IPFSBehaviour, CleanUpBehaviour, ABC):
         self.context.logger.info(
             "local height == remote height; continuing execution..."
         )
-        yield from self.wait_from_last_timestamp(self.params.observation_interval / 2)
+        yield from self.wait_from_last_timestamp(self.sleep_after_hard_reset)
         return True
 
 
