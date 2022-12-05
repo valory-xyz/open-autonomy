0331cf3dcc359bbb510b50ea79b73c4d0831e6f1
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-12-05 19:38:33 +0100

    chore: delete test_fuzz_encode

--- packages/valory/skills/abstract_round_abci/tests/test_serializer.py
+++ packages/valory/skills/abstract_round_abci/tests/test_serializer.py
@@ -39,12 +39,6 @@ from packages.valory.skills.abstract_round_abci.serializer import (
 )
 
 
-try:
-    import atheris  # type: ignore
-except (ImportError, ModuleNotFoundError):
-    atheris: Any = None  # type: ignore
-
-
 PACKAGE_DIR = Path(__file__).parent.parent
 
 
@@ -198,30 +192,6 @@ def test_encode_nan() -> None:
     serialized = serializer.DictProtobufStructSerializer.encode(case)
     deserialized = serializer.DictProtobufStructSerializer.decode(serialized)
     assert math.isnan(deserialized["key"])
-
-
-@pytest.mark.skip
-def test_fuzz_encode() -> None:
-    """Fuzz test for serializer. Run directly as a function, not through pytest"""
-
-    @atheris.instrument_func
-    def test_encode(input_bytes: bytes) -> None:
-        """Test encode decode logic."""
-        fdp = atheris.FuzzedDataProvider(input_bytes)
-        case = {
-            "key1": fdp.ConsumeBool(),
-            "key2": fdp.ConsumeFloat(),
-            "key3": fdp.ConsumeInt(4),
-            "key4": fdp.ConsumeString(12),
-            "key5": fdp.ConsumeBytes(12),
-            "key6": Struct(),
-            "_need_patch": {},
-        }
-        serializer.to_bytes(case)
-
-    atheris.instrument_all()
-    atheris.Setup(sys.argv, test_encode)
-    atheris.Fuzz()
 
 
 def test_encode_non_unicode_raises() -> None:
