3cc487878884919960b6b072e5a967ece308c939
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-12-16 19:29:45 +0100

    fix: restore sys.path, sys.meta_path, invalidate_caches

--- tests/test_autonomy/test_import_all_modules.py
+++ tests/test_autonomy/test_import_all_modules.py
@@ -18,9 +18,10 @@
 # ------------------------------------------------------------------------------
 
 """Test importing all modules"""
-
+import importlib
 import pkgutil
 import sys
+from importlib import reload
 from importlib.machinery import FileFinder, SourceFileLoader
 from typing import cast
 
@@ -32,11 +33,17 @@ import packages
 def test_import_all_modules() -> None:
     """Test importing all modules."""
 
+    old_path = sys.path.copy()
+    old_meta_path = sys.meta_path.copy()
     old_modules = sys.modules.copy()
     try:
         package_names = [autonomy.__name__, packages.__name__]
         for file_finder, module_name, _ in pkgutil.walk_packages(package_names):
             loader = cast(FileFinder, file_finder).find_module(module_name)
             cast(SourceFileLoader, loader).load_module(module_name)
+            cast(FileFinder, file_finder).invalidate_caches()
     finally:
+        sys.path = old_path
+        sys.meta_path = old_meta_path
         sys.modules = old_modules
+        importlib.invalidate_caches()
