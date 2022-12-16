1886bd60bf40f346ce937be2eb44b8279a3974a2
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-12-16 15:57:01 +0100

    tests: test_default_test_class

--- plugins/aea-test-autonomy/tests/test_base_test_classes/test_agents.py
+++ plugins/aea-test-autonomy/tests/test_base_test_classes/test_agents.py
@@ -26,6 +26,7 @@ from aea_test_autonomy.base_test_classes.agents import (
 )
 
 
+
 class BaseTest:
     """BaseTest"""
 
@@ -56,3 +57,25 @@ class BaseTest:
 
         test_instance = self.setup_test()
         assert isinstance(test_instance, self.test_cls)
+
+
+class TestBaseTestEnd2End(BaseTest):
+    """TestBaseTestEnd2End"""
+
+    test_cls = BaseTestEnd2End
+
+    def test_default_test_class(self) -> None:
+        """Test default class attributes, prior to setup_class"""
+
+        # BaseTestEnd2End overrides of BaseAEATestCase
+        assert BaseTestEnd2End.capture_log is True
+        assert BaseTestEnd2End.cli_log_options == ["-v", "DEBUG"]
+
+        # default values likely to change
+        assert BaseTestEnd2End.happy_path == ()
+        assert BaseTestEnd2End.strict_check_strings == ()
+
+        # no overwrite of parent class setup_class -> no tests needed for setup_class
+        child, parent = BaseTestEnd2End, BaseTestEnd2End.__mro__[1]
+        assert not child.__module__.split(".")[0] == parent.__module__.split(".")[0]
+        assert child.setup_class.__func__ == parent.setup_class.__func__
