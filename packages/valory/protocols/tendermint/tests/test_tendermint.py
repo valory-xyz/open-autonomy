9c02609a2a5d8ba9c7c807002dcdb483d541dc41
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-12-19 18:36:20 +0100

    testS: Tendermint protocol dialogues

---
+++ packages/valory/protocols/tendermint/tests/test_tendermint.py
@@ -0,0 +1,172 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2022 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""Tests package for the 'valory/tendermint' protocol."""
+
+from typing import Type
+
+import pytest
+from aea.common import Address
+from aea.mail.base import Envelope
+from aea.protocols.base import Message
+from aea.protocols.dialogue.base import Dialogue as BaseDialogue
+from aea.protocols.dialogue.base import DialogueLabel
+
+from packages.valory.protocols.tendermint import TendermintMessage
+from packages.valory.protocols.tendermint.custom_types import ErrorCode
+from packages.valory.protocols.tendermint.dialogues import (
+    TendermintDialogue,
+    TendermintDialogues,
+)
+
+
+@pytest.mark.parametrize(
+    "msg",
+    [
+        TendermintMessage(
+            performative=TendermintMessage.Performative.REQUEST,
+        ),
+        TendermintMessage(
+            performative=TendermintMessage.Performative.RESPONSE,
+            info="",
+        ),
+        TendermintMessage(
+            performative=TendermintMessage.Performative.ERROR,
+            error_code=ErrorCode.INVALID_REQUEST,
+            error_msg="",
+            error_data={},
+        ),
+    ],
+)
+def test_get_balance_serialization(msg: TendermintMessage):
+    """Test the serialization for 'get_balance' speech-act works."""
+
+    msg.to = "receiver"
+    envelope = Envelope(
+        to=msg.to,
+        sender="sender",
+        message=msg,
+    )
+    envelope_bytes = envelope.encode()
+
+    actual_envelope = Envelope.decode(envelope_bytes)
+    expected_envelope = envelope
+    assert expected_envelope.to == actual_envelope.to
+    assert expected_envelope.sender == actual_envelope.sender
+    assert (
+        expected_envelope.protocol_specification_id
+        == actual_envelope.protocol_specification_id
+    )
+    assert expected_envelope.message != actual_envelope.message
+
+    actual_msg = TendermintMessage.serializer.decode(actual_envelope.message)
+    actual_msg.to = actual_envelope.to
+    actual_msg.sender = actual_envelope.sender
+    expected_msg = msg
+    assert expected_msg == actual_msg
+
+
+class AgentDialogue(TendermintDialogue):
+    """The dialogue class maintains state of a dialogue and manages it."""
+
+    def __init__(
+        self,
+        dialogue_label: DialogueLabel,
+        self_address: Address,
+        role: BaseDialogue.Role,
+        message_class: Type[TendermintMessage],
+    ) -> None:
+        """
+        Initialize a dialogue.
+
+        :param dialogue_label: the identifier of the dialogue
+        :param self_address: the address of the entity for whom this dialogue is maintained
+        :param role: the role of the agent this dialogue is maintained for
+        :param message_class: the message class
+        """
+        TendermintDialogue.__init__(
+            self,
+            dialogue_label=dialogue_label,
+            self_address=self_address,
+            role=role,
+            message_class=message_class,
+        )
+
+
+class AgentDialogues(TendermintDialogues):
+    """The dialogues class keeps track of all dialogues."""
+
+    def __init__(self, self_address: Address) -> None:
+        """
+        Initialize dialogues.
+
+        :param self_address: the address of the entity for whom this dialogue is maintained
+        """
+
+        def role_from_first_message(  # pylint: disable=unused-argument
+            message: Message,  # pylint: disable=redefined-outer-name
+            receiver_address: Address,
+        ) -> BaseDialogue.Role:
+            """Infer the role of the agent from an incoming/outgoing first message
+
+            :param message: an incoming/outgoing first message
+            :param receiver_address: the address of the receiving agent
+            :return: The role of the agent
+            """
+            return TendermintDialogue.Role.AGENT
+
+        TendermintDialogues.__init__(
+            self,
+            self_address=self_address,
+            role_from_first_message=role_from_first_message,
+            dialogue_class=AgentDialogue,
+        )
+
+
+class TestDialogues:
+    """Tests abci dialogues."""
+
+    def setup(self):
+        """Setup"""
+
+        self.agent_dialogues = AgentDialogues("agent_address")
+
+    def test_create_self_initiated(self) -> None:
+        """Test the self initialisation of a dialogue."""
+
+        result = (
+            self.agent_dialogues._create_self_initiated(  # pylint: disable=protected-access
+                dialogue_opponent_addr="dummy_address",
+                dialogue_reference=(str(0), ""),
+                role=TendermintDialogue.Role.AGENT,
+            )
+        )
+        assert isinstance(result, TendermintDialogue)
+        assert result.role == TendermintDialogue.Role.AGENT, "The role must be agent."
+
+    def test_create_opponent_initiated(self) -> None:
+        """Test the opponent initialisation of a dialogue."""
+
+        result = self.agent_dialogues._create_opponent_initiated(  # pylint: disable=protected-access
+            dialogue_opponent_addr="dummy_address",
+            dialogue_reference=(str(0), ""),
+            role=TendermintDialogue.Role.AGENT,
+        )
+        assert isinstance(result, TendermintDialogue)
+        assert result.role == TendermintDialogue.Role.AGENT, "The role must be agent."
