cc1854388db06b32e3d61bbe690e44ec3cd1337b
Karrenbelt <m.a.p.karrenbelt@gmail.com>
2022-12-21 00:00:46 +0100

    chore: linters

--- autonomy/constants.py
+++ autonomy/constants.py
@@ -57,4 +57,4 @@ DEFAULT_SERVICE_REGISTRY_CONTRACTS_IMAGE = (
 ACN_IMAGE_NAME = os.environ.get("ACN_IMAGE_NAME", "valory/open-acn-node")
 
 OAR_IMAGE = "valory/oar-{agent}:{version}"
-ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeidy2wrrxdzicrod6lrsaacio52fgj3ndnaxbc4dipap6zs4yekoue"
+ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeibyn72ayqh7boppa27paem7x6mmhqnabsfjfhrd7hrixfwblywlwi"
--- docs/counter_example.md
+++ docs/counter_example.md
@@ -25,7 +25,7 @@ you have followed the [setup instructions](guides/quick_start.md#setup). As a re
 
 2. Use the CLI to download the `valory/counter` service.
     ```bash
-    autonomy fetch valory/counter:0.1.0:bafybeifq5kyxesbci3vt7v43fjkvbwy2dl7a5m5dpg4rg2qg2ro4sib5za --remote --service
+    autonomy fetch valory/counter:0.1.0:bafybeidtylgkyxpa3n6slsb2nnvi7r3cebdjbelw57iml6xehqrrzxsxwq --remote --service
     cd counter
     ```
 
