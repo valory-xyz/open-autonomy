aca9cf11fb70e7885b27db95cc3cefba08d56179
David Minarsch <david.minarsch@googlemail.com>
2023-01-01 12:08:00 +0100

    chore: revert accidental hash changes

--- autonomy/constants.py
+++ autonomy/constants.py
@@ -57,4 +57,4 @@ DEFAULT_SERVICE_REGISTRY_CONTRACTS_IMAGE = (
 ACN_IMAGE_NAME = os.environ.get("ACN_IMAGE_NAME", "valory/open-acn-node")
 
 OAR_IMAGE = "valory/oar-{agent}:{version}"
-ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeidawn2peg7tlvjwfo52kfwovgmi4iqonrukcysn3r2qnmtfoe4cre"
+ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeibkunyvmp6qsu7rqtwqyitz7wozyxrycnyupyvnq42xpykzttx4ky"
--- docs/advanced_reference/commands/autonomy_fetch.md
+++ docs/advanced_reference/commands/autonomy_fetch.md
@@ -55,5 +55,5 @@ autonomy --registry-path=./packages fetch valory/hello_world:0.1.0 --service --l
 
 Fetch the agent service `hello_world` from a remote registry ([IPFS](https://ipfs.io)):
 ```bash
-autonomy fetch valory/hello_world:0.1.0:bafybeigyohyazssd5krqprdngulxnhd5lvukwc6x2jteco6vixqhsl2jiq --service --remote
+autonomy fetch valory/hello_world:0.1.0:bafybeieggpxfegol2p2hqu5x7umfbxhvualm4xsgipxmjlykixfzaogqf4 --service --remote
 ```
