81c96f30c4fbbf6b76a992939925be02aeacf55e
Viraj Patel <vptl185@gmail.com>
2023-01-10 16:07:33 +0530

    feat: add a contracts helper class

--- autonomy/chain/base.py
+++ autonomy/chain/base.py
@@ -21,9 +21,71 @@
 
 from enum import Enum
 
+from aea.configurations.data_types import PublicId
+from aea.contracts.base import Contract
+from aea.crypto.base import Crypto, LedgerApi
+
+from autonomy.chain.constants import (
+    COMPONENT_REGISTRY_CONTRACT,
+    CONTRACTS_DIR_FRAMEWORK,
+    CONTRACTS_DIR_LOCAL,
+    REGISTRIES_MANAGER_CONTRACT,
+)
+
 
 class UnitType(Enum):
     """Unit type."""
 
     COMPONENT = 0
     AGENT = 1
+
+
+class ContractsHelper:
+    """Autonolas registry contracts helper"""
+
+    def __init__(self) -> None:
+        """Initialize object."""
+
+        self._registries_manager = None
+        self._component_registry = None
+
+    @staticmethod
+    def get_contract(public_id: PublicId) -> Contract:
+        """Load contract for given public id."""
+
+        # check if a local package is available
+        contract_dir = CONTRACTS_DIR_LOCAL / public_id.name
+        if contract_dir.exists():
+            return Contract.from_dir(directory=contract_dir)
+
+        # if local package is not available use one from the data directory
+        contract_dir = CONTRACTS_DIR_FRAMEWORK / public_id.name
+        if not contract_dir.exists():
+            raise FileNotFoundError(
+                "Contract package not found in the distribution, please reinstall the package"
+            )
+        return Contract.from_dir(directory=contract_dir)
+
+    @property
+    def registries_manager(
+        self,
+    ) -> Contract:
+        """Returns an instance of the registries manager contract."""
+        if self._registries_manager is None:
+            self._registries_manager = self.get_contract(
+                public_id=REGISTRIES_MANAGER_CONTRACT
+            )
+
+        return self._registries_manager
+
+    @property
+    def component_registry(
+        self,
+    ) -> Contract:
+        """Returns an instance of the registries manager contract."""
+        if self._component_registry is None:
+            self._component_registry = self.get_contract(
+                public_id=COMPONENT_REGISTRY_CONTRACT
+            )
+
+        return self._component_registry
