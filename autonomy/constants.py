8bef40b4dc5e226918d32b6052023dce73263948
David Minarsch <david.minarsch@googlemail.com>
2023-02-05 17:39:06 +0000

    chore: regenerate latest hashes

--- autonomy/constants.py
+++ autonomy/constants.py
@@ -58,4 +58,4 @@ DEFAULT_SERVICE_REGISTRY_CONTRACTS_IMAGE = (
 ACN_IMAGE_NAME = os.environ.get("ACN_IMAGE_NAME", "valory/open-acn-node")
 
 OAR_IMAGE = "valory/oar-{agent}:{version}"
-ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeicbfb36p74pw7yh7hlasknenba6hhdmjrql7xghw63qq2bvtfoofa"
+ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeibo674yt2vghw5zd3xxm4uvahbcncmzn5ub5styfesod6guhluwn4"
--- docs/counter_example.md
+++ docs/counter_example.md
@@ -25,7 +25,7 @@ you have followed the [setup instructions](guides/quick_start.md#setup). As a re
 
 2. Use the CLI to download the `valory/counter` service.
     ```bash
-    autonomy fetch valory/counter:0.1.0:bafybeih6kqh23n3qeb7r3g64qnkfxnih7mr7alltoe6ha7yq3mx2jmvj4q --remote --service
+    autonomy fetch valory/counter:0.1.0:bafybeihvjdbo5dtvyx4ltvcbvgjo7qxmy45l6zrsmn3pb4ibj7zeaf5whi --remote --service
     cd counter
     ```
 
