9e4710d3eaf46c359912329505fd889699c359fa
Viraj Patel <vptl185@gmail.com>
2023-02-06 14:49:19 +0530

    chore: bump packages

--- autonomy/constants.py
+++ autonomy/constants.py
@@ -58,4 +58,4 @@ DEFAULT_SERVICE_REGISTRY_CONTRACTS_IMAGE = (
 ACN_IMAGE_NAME = os.environ.get("ACN_IMAGE_NAME", "valory/open-acn-node")
 
 OAR_IMAGE = "valory/oar-{agent}:{version}"
-ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeicbfb36p74pw7yh7hlasknenba6hhdmjrql7xghw63qq2bvtfoofa"
+ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeiee7ay6az4lijaqncgtif6clklkmcehe7qwthvdlupksq3otuhp5q"
--- docs/advanced_reference/commands/autonomy_fetch.md
+++ docs/advanced_reference/commands/autonomy_fetch.md
@@ -55,5 +55,5 @@ autonomy --registry-path=./packages fetch valory/hello_world:0.1.0 --service --l
 
 Fetch the agent service `hello_world` from a remote registry ([IPFS](https://ipfs.io)):
 ```bash
-autonomy fetch valory/hello_world:0.1.0:bafybeibb6bnw5rcsejmagpplkt2jrdutwcaqyzqyhukveuztgobmpegyve --service --remote
+autonomy fetch valory/hello_world:0.1.0:bafybeibzqswgbnbmsszpr4u7izljnft5hok4vpghrh645tlpdrunf6ifl4 --service --remote
 ```
