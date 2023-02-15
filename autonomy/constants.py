cd58fab522ec1361e8f862203ad6e384eac8dd0f
Adamantios <adamantios.zaras@valory.xyz>
2023-02-15 19:16:59 +0200

    chore: run generators

--- autonomy/constants.py
+++ autonomy/constants.py
@@ -58,4 +58,4 @@ DEFAULT_SERVICE_REGISTRY_CONTRACTS_IMAGE = (
 ACN_IMAGE_NAME = os.environ.get("ACN_IMAGE_NAME", "valory/open-acn-node")
 DEFAULT_DOCKER_IMAGE_AUTHOR = "valory"
 OAR_IMAGE = "{image_author}/oar-{agent}:{version}"
-ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeiegswtf47hos42fgklo6ovbfyrpm4fprxfuvkylom72cbwcjdq6ca"
+ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeib3mfh3sx6aobbqfakknghjuwh2dstwrqumz4hx66jf5gwsk7cpay"
--- docs/advanced_reference/commands/autonomy_fetch.md
+++ docs/advanced_reference/commands/autonomy_fetch.md
@@ -55,5 +55,5 @@ autonomy --registry-path=./packages fetch valory/hello_world:0.1.0 --service --l
 
 Fetch the agent service `hello_world` from a remote registry ([IPFS](https://ipfs.io)):
 ```bash
-autonomy fetch valory/hello_world:0.1.0:bafybeiddbf42gw2jovlhb6qfsttbpl7clrvj2mo6aajsaff6ntdd63pxii --service --remote
+autonomy fetch valory/hello_world:0.1.0:bafybeidgd36meymqvyl3rqoipbs6m43bskkb3lkxceyp4g2pxzd64bmeai --service --remote
 ```
