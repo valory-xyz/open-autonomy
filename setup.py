67b6758d5e66461d876eef18271fed8071cbd3c2
angrybayblade <vptl185@gmail.com>
2023-03-10 10:44:25 +0530

    feat: add hardware wallet plugin as a dependency for the autonomy framework

--- setup.py
+++ setup.py
@@ -56,6 +56,7 @@ base_deps = [
     "watchdog >=2.1.6",
     "pytest==7.2.1",
     "open-aea-ledger-ethereum==1.30.0",
+    "open-aea-ledger-ethereum-hwi>=1.30.0,<2.0.0",
     "docker-compose==1.29.2",
     "werkzeug==2.0.3",
     "docker==6.0.0",
