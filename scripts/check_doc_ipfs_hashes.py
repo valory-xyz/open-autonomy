09ae6dfde52ea15f52a787d9031846588d5007de
angrybayblade <vptl185@gmail.com>
2023-04-24 07:10:38 +0000

    fix: update hash update script to fix hashes in `set_up.md` file

--- scripts/check_doc_ipfs_hashes.py
+++ scripts/check_doc_ipfs_hashes.py
@@ -32,7 +32,7 @@ import yaml
 from aea.cli.packages import get_package_manager
 from aea.configurations.data_types import PackageId
 from aea.helpers.base import IPFS_HASH_REGEX, SIMPLE_ID_REGEX
-
+import json
 
 CLI_REGEX = r"(?P<cli>aea|autonomy)"
 # CMD_REGEX should be r"(?P<cmd>(\S+\s(\s--\S+)*)+)",
@@ -239,6 +239,38 @@ class PackageHashManager:
         return self.package_tree[vendor][package_type][package_name].hash
 
 
+def update_set_up(fix: bool = False) -> None:
+    """Update `set_up.md` file."""
+    md_file = Path("docs", "guides", "set_up.md")
+    packages: Dict[str, Dict[str, str]] = json.loads(
+        Path("packages", "packages.json").read_text()
+    )
+    package_hash_str_re = re.compile(
+        r"(\"([a-z_0-9]+/[a-z_0-9]+/[a-z_0-9]+/[0-9.]+)\": \"(bafybei[0-9a-z]+)\")"
+    )
+    md_file_content = md_file.read_text()
+    for match in package_hash_str_re.findall(md_file_content):
+        line, package_name, package_hash = match
+        package_hash_update = packages.get("dev").get(
+            package_name, packages.get("third_party").get(package_name)
+        )
+        if package_hash == package_hash_update:
+            continue
+        if not fix:
+            print(
+                f"Package hash does not match in `docs/guides/set_up.md` file"
+                f"\n\tPackage name: {package_name}"
+                f"\n\tHash in file: {package_hash}"
+                f"\n\tHash in `packages.json`: {package_hash_update}"
+            )
+            sys.exit(1)
+
+        line_update = line.replace(package_hash, package_hash_update)
+        md_file_content = md_file_content.replace(line, line_update)
+
+    md_file.write_text(md_file_content)
+
+
 def check_ipfs_hashes(  # pylint: disable=too-many-locals,too-many-statements
     paths: Optional[List[Path]] = None, fix: bool = False
 ) -> None:
@@ -385,6 +417,8 @@ def check_ipfs_hashes(  # pylint: disable=too-many-locals,too-many-statements
             "No commands were found in the docs. The command regex is probably outdated."
         )
         sys.exit(1)
+
+    update_set_up(fix=fix)
 
     print("Checking doc IPFS hashes finished successfully.")
 
