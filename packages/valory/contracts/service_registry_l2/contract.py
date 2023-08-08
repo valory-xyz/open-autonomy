69dffa847a15f47503dfb436fa93374d7b2e19cd
angrybayblade <vptl185@gmail.com>
2023-08-08 12:04:26 +0000

    feat: service registry L2 contract

---
+++ packages/valory/contracts/service_registry_l2/contract.py
@@ -0,0 +1,295 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2022-2023 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""This module contains the class to connect to the Service Registry contract."""
+
+import hashlib
+import logging
+from typing import Any, Dict, List, Optional, Set, Tuple, Union, cast
+
+from aea.common import JSONLike
+from aea.configurations.base import PublicId
+from aea.contracts.base import Contract
+from aea_ledger_ethereum import EthereumApi, LedgerApi
+
+
+EXPECTED_CONTRACT_ADDRESS_BY_CHAIN_ID = {
+    1: "0x48b6af7B12C71f09e2fC8aF4855De4Ff54e775cA",
+    5: "0x1cEe30D08943EB58EFF84DD1AB44a6ee6FEff63a",
+    100: "0x9338b5153AE39BB89f50468E608eD9d764B755fD",
+    137: "0xE3607b00E75f6405248323A9417ff6b39B244b50",
+    31337: "0x998abeb3E57409262aE5b751f60747921B33613E",
+}
+DEPLOYED_BYTECODE_MD5_HASH_BY_CHAIN_ID = {
+    1: "6f9fc7f3c2348801737120e6b5f8fa8e9670c65152c66d128ff4cddb465b4d705340c559e352f5e7f29bda3b84a8d36d4a9448b791cfe2d370e31c01276e0244",
+    5: "d4a860f21f17762c99d93359244b39a878dd5bac9ea6056c0ff29c7558d6653aa0d5962aa819fc9f05f237d068845125cfc37a7fd7761b11c29a709ad5c48157",
+    100: "10e2cfb500481d6c5a3b6b90507e4ac04d8b0d88741cea5568306ed4115f24e8b9747055423da5fca05838d5ccefebf41fb47d2ba1fb45215b6b21c0a27823be",
+    137: "10e2cfb500481d6c5a3b6b90507e4ac04d8b0d88741cea5568306ed4115f24e8b9747055423da5fca05838d5ccefebf41fb47d2ba1fb45215b6b21c0a27823be",
+    31337: "d8084598f884509694ab1f244cbb8e7697d8db00c241710b89b2ec3037d2edd3b82d01f1f0ca6bd9b265b1184d9c563a6000c30958c2a7ae5a9c35e5ff2ba7de",
+}
+UNIT_HASH_PREFIX = "0x{metadata_hash}"
+
+PUBLIC_ID = PublicId.from_str("valory/service_registry:0.1.0")
+
+_logger = logging.getLogger(
+    f"aea.packages.{PUBLIC_ID.author}.contracts.{PUBLIC_ID.name}.contract"
+)
+
+ServiceInfo = Tuple[int, str, bytes, int, int, int, int, List[int]]
+
+
+class ServiceRegistryL2Contract(Contract):
+    """The Service Registry contract."""
+
+    contract_id = PUBLIC_ID
+
+    @classmethod
+    def get_raw_transaction(
+        cls, ledger_api: LedgerApi, contract_address: str, **kwargs: Any
+    ) -> Optional[JSONLike]:
+        """Get the Safe transaction."""
+        raise NotImplementedError  # pragma: nocover
+
+    @classmethod
+    def get_raw_message(
+        cls, ledger_api: LedgerApi, contract_address: str, **kwargs: Any
+    ) -> Optional[bytes]:
+        """Get raw message."""
+        raise NotImplementedError  # pragma: nocover
+
+    @classmethod
+    def get_state(
+        cls, ledger_api: LedgerApi, contract_address: str, **kwargs: Any
+    ) -> Optional[JSONLike]:
+        """Get state."""
+        raise NotImplementedError  # pragma: nocover
+
+    @classmethod
+    def verify_contract(
+        cls, ledger_api: LedgerApi, contract_address: str
+    ) -> Dict[str, Union[bool, str]]:
+        """
+        Verify the contract's bytecode
+
+        :param ledger_api: the ledger API object
+        :param contract_address: the contract address
+        :return: the verified status
+        """
+        verified = False
+        ledger_api = cast(EthereumApi, ledger_api)
+        chain_id = ledger_api.api.eth.chain_id
+        expected_address = EXPECTED_CONTRACT_ADDRESS_BY_CHAIN_ID[chain_id]
+        if contract_address != expected_address:
+            _logger.error(
+                f"For chain_id {chain_id} expected {expected_address} and got {contract_address}."
+            )
+            return dict(verified=verified)
+        deployed_bytecode = ledger_api.api.eth.get_code(contract_address).hex()
+        sha512_hash = hashlib.sha512(deployed_bytecode.encode("utf-8")).hexdigest()
+        verified = DEPLOYED_BYTECODE_MD5_HASH_BY_CHAIN_ID[chain_id] == sha512_hash
+        if not verified:
+            _logger.error(
+                f"CONTRACT NOT VERIFIED! Contract address: {contract_address}, chain_id: {chain_id}."
+            )
+        return dict(verified=verified)
+
+    @classmethod
+    def exists(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+        service_id: int,
+    ) -> bool:
+        """Check if the service id exists"""
+
+        contract_instance = cls.get_instance(ledger_api, contract_address)
+        exists = ledger_api.contract_method_call(
+            contract_instance=contract_instance,
+            method_name="exists",
+            unitId=service_id,
+        )
+
+        return cast(bool, exists)
+
+    @classmethod
+    def get_agent_instances(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+        service_id: int,
+    ) -> Dict[str, Any]:
+        """Retrieve on-chain agent instances."""
+
+        contract_instance = cls.get_instance(ledger_api, contract_address)
+        service_info = ledger_api.contract_method_call(
+            contract_instance=contract_instance,
+            method_name="getAgentInstances",
+            serviceId=service_id,
+        )
+
+        return dict(
+            numAgentInstances=service_info[0],
+            agentInstances=service_info[1],
+        )
+
+    @classmethod
+    def get_service_owner(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+        service_id: int,
+    ) -> Dict[str, Any]:
+        """Retrieve the service owner."""
+        contract_instance = cls.get_instance(ledger_api, contract_address)
+        service_owner = contract_instance.functions.ownerOf(service_id).call()
+        checksum_service_owner = ledger_api.api.toChecksumAddress(service_owner)
+        return dict(
+            service_owner=checksum_service_owner,
+        )
+
+    @classmethod
+    def get_service_information(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+        token_id: int,
+    ) -> ServiceInfo:
+        """Retrieve service information"""
+
+        contract_interface = cls.get_instance(
+            ledger_api=ledger_api,
+            contract_address=contract_address,
+        )
+        return contract_interface.functions.getService(token_id).call()
+
+    @classmethod
+    def get_token_uri(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+        token_id: int,
+    ) -> str:
+        """Resolve token URI"""
+
+        contract_interface = cls.get_instance(
+            ledger_api=ledger_api,
+            contract_address=contract_address,
+        )
+        return contract_interface.functions.tokenURI(token_id).call()
+
+    @classmethod
+    def filter_token_id_from_emitted_events(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+    ) -> Optional[int]:
+        """Returns `CreateUnit` event filter."""
+
+        contract_interface = cls.get_instance(
+            ledger_api=ledger_api,
+            contract_address=contract_address,
+        )
+
+        events = contract_interface.events.CreateService.createFilter(
+            fromBlock="latest"
+        ).get_all_entries()
+
+        for event in events:
+            event_args = event["args"]
+            if "serviceId" in event_args:
+                return cast(int, event_args["serviceId"])
+
+        return None
+
+    @classmethod
+    def verify_service_has_been_activated(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+        service_id: int,
+    ) -> bool:
+        """Checks for a successful service registration event in the latest block"""
+
+        contract_interface = cls.get_instance(
+            ledger_api=ledger_api,
+            contract_address=contract_address,
+        )
+
+        events = contract_interface.events.ActivateRegistration.createFilter(
+            fromBlock="latest"
+        ).get_all_entries()
+        for event in events:
+            if event["args"]["serviceId"] == service_id:
+                return True
+
+        return False
+
+    @classmethod
+    def verify_agent_instance_registration(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+        service_id: int,
+        instance_check: Set[str],
+    ) -> Set[str]:
+        """Checks for the registered instances and filters out the instances that are registered from the given array"""
+
+        contract_interface = cls.get_instance(
+            ledger_api=ledger_api,
+            contract_address=contract_address,
+        )
+
+        events = contract_interface.events.RegisterInstance.createFilter(
+            fromBlock="latest"
+        ).get_all_entries()
+
+        successful = set()
+        for event in events:
+            event_args = event["args"]
+            if event_args["serviceId"] != service_id:
+                continue
+
+            agent_instance = event_args["agentInstance"]
+            if agent_instance in instance_check:
+                successful.add(agent_instance)
+
+        return successful
+
+    @classmethod
+    def verify_service_has_been_deployed(
+        cls,
+        ledger_api: LedgerApi,
+        contract_address: str,
+        service_id: int,
+    ) -> bool:
+        """Checks for a successful service registration event in the latest block"""
+
+        contract_interface = cls.get_instance(
+            ledger_api=ledger_api,
+            contract_address=contract_address,
+        )
+
+        events = contract_interface.events.DeployService.createFilter(
+            fromBlock="latest"
+        ).get_all_entries()
+        for event in events:
+            if event["args"]["serviceId"] == service_id:
+                return True
+
+        return False
---
+++ packages/valory/contracts/service_registry_l2/tests/test_contract.py
@@ -0,0 +1,305 @@
+# -*- coding: utf-8 -*-
+# ------------------------------------------------------------------------------
+#
+#   Copyright 2022-2023 Valory AG
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+# ------------------------------------------------------------------------------
+
+"""Tests for valory/service_registry contract."""
+from pathlib import Path
+from typing import Any, List, Optional, Set
+from unittest import mock
+
+import pytest
+from aea_ledger_ethereum import EthereumCrypto
+from aea_test_autonomy.base_test_classes.contracts import BaseRegistriesContractsTest
+from aea_test_autonomy.docker.base import skip_docker_tests
+
+from packages.valory.contracts.service_registry_l2.contract import (
+    DEPLOYED_BYTECODE_MD5_HASH_BY_CHAIN_ID,
+    EXPECTED_CONTRACT_ADDRESS_BY_CHAIN_ID,
+    ServiceRegistryL2Contract,
+)
+
+
+PACKAGE_DIR = Path(__file__).parent.parent
+
+SERVICE_REGISTRY_INVALID = "0x5aAeb6053F3E94C9b9A09f33669435E7Ef1BeAed"
+VALID_SERVICE_ID = 1
+INVALID_SERVICE_ID = 0
+CHAIN_ID = 31337
+
+
+def event_filter_patch(event: str, return_value: Any) -> mock._patch:
+    """Returns an event filter patch for the given event name."""
+    return mock.patch.object(
+        ServiceRegistryL2Contract,
+        "get_instance",
+        return_value=mock.MagicMock(
+            events=mock.MagicMock(
+                **{
+                    event: mock.MagicMock(
+                        createFilter=lambda **_: mock.MagicMock(
+                            get_all_entries=lambda *_: return_value
+                        )
+                    )
+                }
+            )
+        ),
+    )
+
+
+class BaseServiceRegistryL2ContractTest(BaseRegistriesContractsTest):
+    """Base class for Service Registry contract tests"""
+
+    contract: ServiceRegistryL2Contract
+    contract_address = EXPECTED_CONTRACT_ADDRESS_BY_CHAIN_ID[CHAIN_ID]
+    invalid_contract_address = SERVICE_REGISTRY_INVALID
+    path_to_contract = PACKAGE_DIR
+    ledger_identifier = EthereumCrypto.identifier
+    contract_directory = PACKAGE_DIR
+
+
+@skip_docker_tests
+class TestServiceRegistryL2Contract(BaseServiceRegistryL2ContractTest):
+    """Test Service Registry Contract"""
+
+    @pytest.mark.parametrize("valid_address", (True, False))
+    def test_verify_contract(self, valid_address: bool) -> None:
+        """Run verify test. If abi file is updated tests + addresses need updating"""
+        bytecode = DEPLOYED_BYTECODE_MD5_HASH_BY_CHAIN_ID[CHAIN_ID]
+
+        if valid_address:
+            contract_address = self.contract_address
+        else:
+            contract_address = self.invalid_contract_address
+            bytecode += "invalid"
+
+        result = self.contract.verify_contract(
+            self.ledger_api,
+            contract_address,
+        )
+
+        assert result["verified"] is valid_address, result
+
+    @pytest.mark.parametrize(
+        "service_id, expected", [(INVALID_SERVICE_ID, False), (VALID_SERVICE_ID, True)]
+    )
+    def test_exists(self, service_id: int, expected: int) -> None:
+        """Test whether service id exists"""
+        exists = self.contract.exists(
+            self.ledger_api,
+            self.contract_address,
+            service_id,
+        )
+
+        assert exists is expected
+
+    def test_get_agent_instances(self) -> None:
+        """Test agent instances retrieval"""
+
+        return_value = {
+            "numAgentInstances": 4,
+            "agentInstances": [
+                "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266",
+                "0x70997970C51812dc3A010C7d01b50e0d17dc79C8",
+                "0x3C44CdDdB6a900fa2b585dd299e03d12FA4293BC",
+                "0x90F79bf6EB2c4f870365E785982E1f101E93b906",
+            ],
+        }
+
+        assert self.contract_address is not None
+
+        result = self.contract.get_agent_instances(
+            self.ledger_api,
+            self.contract_address,
+            VALID_SERVICE_ID,
+        )
+
+        assert result == return_value
+
+    def test_get_service_owner(self) -> None:
+        """Test service owner retrieval."""
+        service_owner = "0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266"
+        assert self.contract_address is not None
+
+        actual = self.contract.get_service_owner(
+            self.ledger_api,
+            self.contract_address,
+            VALID_SERVICE_ID,
+        )
+
+        expected = dict(service_owner=service_owner)
+        assert expected == actual
+
+    def test_get_token_uri(self) -> None:
+        """Test `get_token_uri` method."""
+
+        token_uri = self.contract.get_token_uri(
+            self.ledger_api,
+            self.contract_address,
+            VALID_SERVICE_ID,
+        )
+
+        assert (
+            token_uri
+            == "https://gateway.autonolas.tech/ipfs/f017012205555555555555555555555555555555555555555555555555555555555555555"  # nosec
+        )
+
+    def test_get_service_information(self) -> None:
+        """Test `test_get_service_information` method."""
+
+        (
+            security_deposit,
+            multisig_address,
+            ipfs_hash_for_config,
+            threshold,
+            max_number_of_agent_instances,
+            number_of_agent_instances,
+            service_state,
+            list_of_cannonical_agents,
+        ) = self.contract.get_service_information(
+            self.ledger_api,
+            self.contract_address,
+            VALID_SERVICE_ID,
+        )
+
+        assert security_deposit == 10000000000000000
+        assert multisig_address == "0xD8dE647170163a981bb3Fdb2063583eAcF7D55AC"
+        assert ipfs_hash_for_config == b"UUUUUUUUUUUUUUUUUUUUUUUUUUUUUUUU"
+        assert threshold == 3
+        assert max_number_of_agent_instances == 4
+        assert number_of_agent_instances == 4
+        assert service_state == 4
+        assert list_of_cannonical_agents == [1]
+
+    @pytest.mark.parametrize(
+        ("return_value", "assert_value"),
+        (
+            ([], None),
+            (
+                [
+                    {
+                        "args": {
+                            "serviceId": 1,
+                        }
+                    }
+                ],
+                1,
+            ),
+        ),
+    )
+    def test_filter_token_id_from_emitted_events(
+        self, return_value: List, assert_value: Optional[int]
+    ) -> None:
+        """Test `filter_token_id_from_emitted_events` method"""
+
+        with event_filter_patch(event="CreateService", return_value=return_value):
+            token_id = self.contract.filter_token_id_from_emitted_events(
+                ledger_api=self.ledger_api,
+                contract_address=self.contract_address,
+            )
+
+            if assert_value is None:
+                assert token_id is None
+            else:
+                assert token_id == 1
+
+    @pytest.mark.parametrize(
+        ("return_value", "assert_value"),
+        (
+            ([], False),
+            (
+                [
+                    {
+                        "args": {
+                            "serviceId": 0,
+                        }
+                    }
+                ],
+                True,
+            ),
+        ),
+    )
+    def test_verify_service_has_been_activated(
+        self, return_value: List, assert_value: bool
+    ) -> None:
+        """Test `verify_service_has_been_activated` method."""
+
+        with event_filter_patch(
+            event="ActivateRegistration", return_value=return_value
+        ):
+            success = self.contract.verify_service_has_been_activated(
+                ledger_api=self.ledger_api,
+                contract_address=self.contract_address,
+                service_id=0,
+            )
+
+            assert success is assert_value
+
+    @pytest.mark.parametrize(
+        ("return_value", "assert_value"),
+        (
+            ([], set()),
+            (
+                [{"args": {"serviceId": 0, "agentInstance": "0x"}}],
+                {"0x"},
+            ),
+        ),
+    )
+    def test_verify_agent_instance_registration(
+        self, return_value: List, assert_value: Set[str]
+    ) -> None:
+        """Test `verify_agent_instance_registration` method."""
+
+        with event_filter_patch(event="RegisterInstance", return_value=return_value):
+            successful = self.contract.verify_agent_instance_registration(
+                ledger_api=self.ledger_api,
+                contract_address=self.contract_address,
+                service_id=0,
+                instance_check={"0x"},
+            )
+
+            assert successful == assert_value
+
+    @pytest.mark.parametrize(
+        ("return_value", "assert_value"),
+        (
+            ([], False),
+            (
+                [
+                    {
+                        "args": {
+                            "serviceId": 0,
+                        }
+                    }
+                ],
+                True,
+            ),
+        ),
+    )
+    def test_verify_service_has_been_deployed(
+        self, return_value: List, assert_value: bool
+    ) -> None:
+        """Test `verify_service_has_been_deployed` method."""
+
+        with event_filter_patch(event="DeployService", return_value=return_value):
+            success = self.contract.verify_service_has_been_deployed(
+                ledger_api=self.ledger_api,
+                contract_address=self.contract_address,
+                service_id=0,
+            )
+
+            assert success is assert_value
