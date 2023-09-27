4ec0db4bd207c39c92b8288537e4b27873783965
angrybayblade <vptl185@gmail.com>
2023-09-27 12:22:10 +0530

    feat: bump open-autonomy@v0.13.0

--- tests/test_base.py
+++ tests/test_base.py
@@ -62,7 +62,7 @@ def get_test_files(package_type: PackageType) -> List[Path]:
 
 def test_version() -> None:
     """Test the version."""
-    assert autonomy.__version__ == "0.12.1.post4"
+    assert autonomy.__version__ == "0.13.0"
 
 
 @pytest.mark.parametrize(
