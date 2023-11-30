952ad7a3fe8a16a94ba36404fd750a045527a0c8
angrybayblade <vptl185@gmail.com>
2023-11-30 11:33:50 +0530

    feat: update bump script to scan for individual package versions from github repos

--- scripts/bump.py
+++ scripts/bump.py
@@ -8,6 +8,8 @@ This script
 - Performs the packages sync
 """
 
+import os
+import re
 import typing as t
 from pathlib import Path
 
@@ -17,7 +19,7 @@ from aea.cli.utils.click_utils import PyPiDependency
 from aea.configurations.constants import PACKAGES, PACKAGE_TYPE_TO_CONFIG_FILE
 from aea.configurations.data_types import Dependency
 from aea.helpers.logging import setup_logger
-from aea.helpers.yaml_utils import yaml_dump_all, yaml_load_all, yaml_dump, yaml_load
+from aea.helpers.yaml_utils import yaml_dump, yaml_dump_all, yaml_load, yaml_load_all
 from aea.package_manager.v1 import PackageManagerV1
 
 from autonomy.cli.helpers.ipfs_hash import load_configuration
@@ -27,35 +29,87 @@ BUMP_BRANCH = "chore/bump"
 PIPFILE = Path.cwd() / "Pipfile"
 PYPROJECT_TOML = Path.cwd() / "pyproject.toml"
 TOX_INI = Path.cwd() / "tox.ini"
+
 TAGS_URL = "https://api.github.com/repos/{repo}/tags"
+FILE_URL = "https://raw.githubusercontent.com/{repo}/{tag}/{file}"
+
+VERISON_RE = re.compile(r"(__version__|version)( )?=( )?\"(?P<version>[0-9a-z\.]+)\"")
+
 OPEN_AEA_REPO = "valory-xyz/open-aea"
 OPEN_AUTONOMY_REPO = "valory-xyz/open-autonomy"
 
+DEPENDENCY_SPECS = {
+    "open-aea": {
+        "repo": OPEN_AEA_REPO,
+        "file": "aea/__version__.py",
+    },
+    "open-aea-ledger-ethereum": {
+        "repo": OPEN_AEA_REPO,
+        "file": "plugins/aea-ledger-ethereum/setup.py",
+    },
+    "open-aea-ledger-ethereum-flashbots": {
+        "repo": OPEN_AEA_REPO,
+        "file": "plugins/aea-ledger-ethereum-flashbots/setup.py",
+    },
+    "open-aea-ledger-ethereum-hwi": {
+        "repo": OPEN_AEA_REPO,
+        "file": "plugins/aea-ledger-ethereum-hwi/setup.py",
+    },
+    "open-aea-ledger-cosmos": {
+        "repo": OPEN_AEA_REPO,
+        "file": "plugins/aea-ledger-cosmos/setup.py",
+    },
+    "open-aea-ledger-solana": {
+        "repo": OPEN_AEA_REPO,
+        "file": "plugins/aea-ledger-solana/setup.py",
+    },
+    "open-aea-cli-ipfs": {
+        "repo": OPEN_AEA_REPO,
+        "file": "plugins/aea-cli-ipfs/setup.py",
+    },
+    "open-autonomy": {
+        "repo": OPEN_AUTONOMY_REPO,
+        "file": "autonomy/__version__.py",
+    },
+    "open-aea-test-autonomy": {
+        "repo": OPEN_AUTONOMY_REPO,
+        "file": "plugins/aea-test-autonomy/setup.py",
+    },
+}
+
 _cache_file = Path.home() / ".aea" / ".gitcache"
-_repo_version_cache = {}
+_version_cache = {}
 _logger = setup_logger("bump")
 
 
-def load_git_cache():
+def load_git_cache() -> None:
     """Load versions cache."""
     if not _cache_file.exists():
         return
     with _cache_file.open("r", encoding="utf-8") as stream:
-        _repo_version_cache.update(yaml_load(stream=stream))
+        _version_cache.update(yaml_load(stream=stream))
 
 
-def dump_git_cache() -> t.Dict:
+def dump_git_cache() -> None:
     """Dump versions cache."""
     with _cache_file.open("w", encoding="utf-8") as stream:
-        yaml_dump(data=_repo_version_cache, stream=stream)
+        yaml_dump(data=_version_cache, stream=stream)
+
+
+def make_git_request(url: str) -> requests.Response:
+    """Make git request"""
+    auth = os.environ.get("GITHUB_AUTH")
+    if auth is None:
+        return requests.get(url=url)
+    return requests.get(url=url, headers={"Authorization": f"Bearer {auth}"})
 
 
 def get_latest_tag(repo: str) -> str:
     """Fetch latest git tag."""
-    if repo in _repo_version_cache:
-        return _repo_version_cache[repo]
+    if repo in _version_cache:
+        return _version_cache[repo]
 
-    response = requests.get(url=TAGS_URL.format(repo=repo))
+    response = make_git_request(url=TAGS_URL.format(repo=repo))
     if response.status_code != 200:
         raise ValueError(
             f"Fetching tags from `{repo}` failed with message '"
@@ -63,31 +117,43 @@ def get_latest_tag(repo: str) -> str:
             + "'"
         )
     latest_tag_data, *_ = response.json()
-    _repo_version_cache[repo] = latest_tag_data["name"]
-    return _repo_version_cache[repo]
+    _version_cache[repo] = latest_tag_data["name"]
+    return _version_cache[repo]
 
 
-def get_open_aea_dependencies() -> t.Dict[str, str]:
-    """Get open-aea dependencies."""
-    version = get_latest_tag(OPEN_AEA_REPO).replace("v", "")
-    return {
-        "open-aea": f"=={version}",
-        "open-aea-ledger-ethereum": f"=={version}",
-        "open-aea-ledger-ethereum-flashbots": f"=={version}",
-        "open-aea-ledger-ethereum-hwi": f"=={version}",
-        "open-aea-ledger-cosmos": f"=={version}",
-        "open-aea-ledger-solana": f"=={version}",
-        "open-aea-cli-ipfs": f"=={version}",
-    }
+def get_dependency_version(repo: str, file: str) -> str:
+    """Get version spec ."""
+    response = make_git_request(
+        FILE_URL.format(
+            repo=repo,
+            tag=get_latest_tag(repo=repo),
+            file=file,
+        )
+    )
+    if response.status_code != 200:
+        raise ValueError(
+            f"Fetching packages from `{repo}` failed with message '"
+            + response.text
+            + "'"
+        )
+    ((*_, version),) = VERISON_RE.findall(response.content.decode())
+    return f"=={version}"
 
 
-def get_open_autonomy_dependencies() -> t.Dict[str, str]:
-    """Get open-autonomy dependencies."""
-    version = get_latest_tag(OPEN_AUTONOMY_REPO).replace("v", "")
-    return {
-        "open-autonomy": f"=={version}",
-        "open-aea-test-autonomy": f"=={version}",
-    }
+def get_dependencies() -> t.Dict:
+    """Get dependency->version mapping."""
+    dependencies = {}
+    for dependency, specs in DEPENDENCY_SPECS.items():
+        version = _version_cache.get(
+            dependency,
+            get_dependency_version(
+                repo=specs["repo"],
+                file=specs["file"],
+            ),
+        )
+        dependencies[dependency] = version
+    _version_cache.update(dependencies)
+    return dependencies
 
 
 def bump_pipfile_or_pyproject(file: Path, dependencies: t.Dict[str, str]) -> None:
@@ -188,8 +254,7 @@ def main(extra: t.List[Dependency], sync: bool, no_cache: bool) -> None:
         load_git_cache()
 
     dependencies = {}
-    dependencies.update(get_open_aea_dependencies())
-    dependencies.update(get_open_autonomy_dependencies())
+    dependencies.update(get_dependencies())
     dependencies.update({dep.name: dep.version for dep in extra or []})
 
     bump_pipfile_or_pyproject(PIPFILE, dependencies=dependencies)
@@ -204,8 +269,8 @@ def main(extra: t.List[Dependency], sync: bool, no_cache: bool) -> None:
         )
         pm.sync(
             sources=[
-                f"{OPEN_AEA_REPO}:{_repo_version_cache[OPEN_AEA_REPO]}",
-                f"{OPEN_AUTONOMY_REPO}:{_repo_version_cache[OPEN_AUTONOMY_REPO]}",
+                f"{OPEN_AEA_REPO}:{_version_cache[OPEN_AEA_REPO]}",
+                f"{OPEN_AUTONOMY_REPO}:{_version_cache[OPEN_AUTONOMY_REPO]}",
             ]
         )
         pm.update_package_hashes()
