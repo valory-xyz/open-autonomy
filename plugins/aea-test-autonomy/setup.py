712b194df17a2014cacf825624937b65504b4123
angrybayblade <vptl185@gmail.com>
2023-12-19 19:23:17 +0530

    chore: bump open-aea@1.43.0.post1

--- plugins/aea-test-autonomy/setup.py
+++ plugins/aea-test-autonomy/setup.py
@@ -24,9 +24,9 @@ from setuptools import setup  # type: ignore
 
 
 base_deps = [
-    "open-aea[all]>=1.43.0,<2.0.0",
+    "open-aea[all]>=1.43.0.post1,<2.0.0",
     "pytest==7.2.1",
-    "open-aea-ledger-ethereum>=1.43.0,<2.0.0",
+    "open-aea-ledger-ethereum>=1.43.0.post1,<2.0.0",
     "docker==6.1.2",
 ]
 
