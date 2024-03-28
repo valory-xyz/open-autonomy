ed957e06b8b597292b6cd1693eeefd8babf780f9
devanshu <devanshukumar45@gmail.com>
2024-03-28 19:32:22 +0530

    updated check_unreferenced_events

--- autonomy/analyse/abci/app_spec.py
+++ autonomy/analyse/abci/app_spec.py
@@ -475,13 +475,12 @@ def check_unreferenced_events(abci_app_cls: Any) -> List[str]:
                 f"but missing from transition function"
             )
 
-        # Filter timeout events using referenced events since we don't explicitly return the timeout events
-        timeout_events = round_transition_events - referenced_events
-        missing_timeout_events = timeout_events - abci_app_timeout_events
-        if len(missing_timeout_events) > 0:
+        # Filter unreferenced events using referenced events since we don't explicitly return the unreferenced events
+        unreferenced_events = (round_transition_events - referenced_events) - abci_app_timeout_events
+        if len(unreferenced_events) > 0:
             error_strings.append(
-                f"Events {missing_timeout_events} are defined in the round transitions of `{round_cls.__name__}` "
-                f"but not in `event_to_timeout`"
+                f"Events {unreferenced_events} are defined in the round transitions of `{round_cls.__name__}` "
+                f"but not in {round_cls.__name__}"
             )
 
     return error_strings
