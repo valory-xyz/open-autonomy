1311ab35f3c62aa31551332c3bd252f1d44e1344
Adamantios <adamantios.zaras@valory.xyz>
2024-07-10 17:45:37 +0300

    chore: run generators

--- autonomy/constants.py
+++ autonomy/constants.py
@@ -65,4 +65,4 @@ DEFAULT_SERVICE_REGISTRY_CONTRACTS_IMAGE = (
 ACN_IMAGE_NAME = os.environ.get("ACN_IMAGE_NAME", "valory/open-acn-node")
 DEFAULT_DOCKER_IMAGE_AUTHOR = "valory"
 OAR_IMAGE = "{image_author}/oar-{agent}:{version}"
-ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeigud2sytkb2ca7lwk7qcz2mycdevdh7qy725fxvwioeeqr7xpwq4e"
+ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeia6lemk5s64f26qjnd2746s5mufpzxuaf5frsqhfbr62kk3ma6sp4"
--- docs/counter_example.md
+++ docs/counter_example.md
@@ -25,7 +25,7 @@ you have followed the [setup instructions](guides/quick_start.md#setup). As a re
 
 2. Use the CLI to download the `valory/counter` service.
     ```bash
-    autonomy fetch valory/counter:0.1.0:bafybeih5x5vrpkhzbr2okktefganlpjgrhld7qjchujrllwaqdqtdevyia --remote --service
+    autonomy fetch valory/counter:0.1.0:bafybeiettpqfnhlq7tidkr5lhzkwxtrj2njchoewpqqhiie5jokc2kcqca --remote --service
     cd counter
     ```
 
