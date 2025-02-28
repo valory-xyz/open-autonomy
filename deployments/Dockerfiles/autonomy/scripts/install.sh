6b246b7715569fd2b38d7f9b9f139e868309e63a
8ball030 <35799987+8ball030@users.noreply.github.com>
2025-02-28 06:48:23 +0000

    Re-order-build-install

--- deployments/Dockerfiles/autonomy/scripts/install.sh
+++ deployments/Dockerfiles/autonomy/scripts/install.sh
@@ -12,13 +12,14 @@ echo "Loading $AEA_AGENT"
 aea fetch $AEA_AGENT --alias agent || exit 1
 cd agent
 
+echo "Installing the necessary python dependencies!"
+aea install --timeout 600 $EXTRA_DEPENDENCIES || exit 1
+echo "Successfully Installed the python dependencies."
+
 echo "Building the deployments host dependencies."
 aea build || exit 1
 echo "Successfully built the host dependencies."
 
-echo "Installing the necessary python dependencies!"
-aea install --timeout 600 $EXTRA_DEPENDENCIES || exit 1
-echo "Successfully Installed the python dependencies."
 
 echo "Done."
 cd ..
