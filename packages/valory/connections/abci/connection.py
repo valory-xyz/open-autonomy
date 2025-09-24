882c1b7a586bcb60494d8be066c2a7bcc97cc2a8
Yuri (solarw) Turchenkov <solarw.mail@gmail.com>
2025-09-24 15:10:11 +0300

    tendermint improving logs: fixes

--- packages/valory/connections/abci/connection.py
+++ packages/valory/connections/abci/connection.py
@@ -24,9 +24,11 @@ import os
 import platform
 import signal
 import subprocess  # nosec
+import sys
 from asyncio import AbstractEventLoop, AbstractServer, CancelledError, Task
 from io import BytesIO
 from logging import Logger
+from logging.handlers import RotatingFileHandler
 from pathlib import Path
 from threading import Event, Thread
 from typing import Any, Dict, List, Optional, Tuple, Union, cast
@@ -1143,18 +1145,32 @@ class TendermintNode:
         self,
         params: TendermintParams,
         logger: Optional[Logger] = None,
+        write_to_log: bool = False,
     ):
         """
         Initialize a Tendermint node.
 
         :param params: the parameters.
         :param logger: the logger.
+        :param write_to_log: Write to log file.
         """
         self.params = params
         self._process: Optional[subprocess.Popen] = None
         self._monitoring: Optional[StoppableThread] = None
         self._stopping = False
         self.logger = logger or logging.getLogger()
+        self.log_file = os.environ.get("LOG_FILE", DEFAULT_TENDERMINT_LOG_FILE)
+        self.write_to_log = write_to_log
+
+        if self.write_to_log:
+            max_bytes = int(
+                os.environ.get("LOG_FILE_MAX_BYTES", DEFAULT_LOG_FILE_MAX_BYTES)
+            )
+            handler = RotatingFileHandler(
+                self.log_file, maxBytes=max_bytes, backupCount=1
+            )
+            handler.setFormatter(logging.Formatter("%(message)s"))
+            self.logger.addHandler(handler)
 
     def _build_init_command(self) -> List[str]:
         """Build the 'init' command."""
@@ -1281,9 +1297,17 @@ class TendermintNode:
         self._stop_tm_process()
         self._stop_monitoring_thread()
 
+    @staticmethod
+    def _write_to_console(line: str) -> None:
+        """Write line to console."""
+        sys.stdout.write(str(line))
+        sys.stdout.flush()
+
     def log(self, line: str) -> None:
         """Open and write a line to the log file."""
-        self.logger.info(line.strip())
+        self._write_to_console(line=line)
+        if self.write_to_log:
+            self.logger.info(line.strip())
 
     def prune_blocks(self) -> int:
         """Prune blocks from the Tendermint state"""
