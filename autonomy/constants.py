71b82f1923077523cda97200f0940403b57798e6
Ojuswi Rastogi <ojuswimail@gmail.com>
2025-10-01 18:33:28 +0530

    Chore: Bump version to `0.21.2`
    Signed-off-by: Ojuswi Rastogi <ojuswimail@gmail.com>

--- autonomy/constants.py
+++ autonomy/constants.py
@@ -65,5 +65,5 @@ DEFAULT_SERVICE_REGISTRY_CONTRACTS_IMAGE = (
 ACN_IMAGE_NAME = os.environ.get("ACN_IMAGE_NAME", "valory/open-acn-node")
 DEFAULT_DOCKER_IMAGE_AUTHOR = "valory"
 OAR_IMAGE = "{image_author}/oar-{agent}:{version}"
-ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeiaypthz4rxgw7wgv2tjvtbbvxbw4r6cuyhtgmc6yjnvuxahjtnliy"
+ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeiemzg3e2hc5hjnpqj4kulxq4mjz65ktelqdgb6wvxekiwudjp63uu"
 OLAS_DOCS_URL = "https://stack.olas.network"
--- tests/test_base.py
+++ tests/test_base.py
@@ -62,7 +62,7 @@ def get_test_files(package_type: PackageType) -> List[Path]:
 
 def test_version() -> None:
     """Test the version."""
-    assert autonomy.__version__ == "0.21.1"
+    assert autonomy.__version__ == "0.21.2"
 
 
 @pytest.mark.parametrize(
