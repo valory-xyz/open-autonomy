f31fe028e9abaa30a2e543083b311e884be11a89
Ojuswi Rastogi <ojuswimail@gmail.com>
2025-11-26 18:04:11 +0530

    revert fix: type checking if from service_registry
    Signed-off-by: Ojuswi Rastogi <ojuswimail@gmail.com>

--- packages/valory/contracts/service_registry/contract.py
+++ packages/valory/contracts/service_registry/contract.py
@@ -22,26 +22,13 @@
 import json
 import logging
 from pathlib import Path
-from typing import (
-    Any,
-    Dict,
-    FrozenSet,
-    List,
-    Optional,
-    TYPE_CHECKING,
-    Tuple,
-    Union,
-    cast,
-)
+from typing import Any, Dict, FrozenSet, List, Optional, Tuple, Union, cast
 
 from aea.common import JSONLike
 from aea.configurations.base import PublicId
 from aea.contracts.base import Contract
 from aea.crypto.base import LedgerApi
-
-
-if TYPE_CHECKING:
-    from web3.types import BlockData, EventData, TxReceipt
+from web3.types import BlockData, EventData, TxReceipt
 
 
 PUBLIC_ID = PublicId.from_str("valory/service_registry:0.1.0")
