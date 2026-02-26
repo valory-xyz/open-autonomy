1de72f885e6a7e80073197fea4e32593b9ee64a5
jmoreira-valory <jose.moreira.sanchez@valory.xyz>
2026-02-26 12:45:50 +0100

    fix: linters

--- tests/test_docs/test_code_blocks.py
+++ tests/test_docs/test_code_blocks.py
@@ -179,7 +179,6 @@ class TestYamlSnippets(BaseTestDocCode):
         "docs/configure_service/configure_access_external_chains.md",  # just placeholder examples
         "docs/advanced_reference/developer_tooling/dev_mode.md",  # just placeholder examples
         "docs/advanced_reference/commands/autonomy_deploy.md",  # blocks contain resource values on deployment configurations
-        "docs/advanced_reference/testing_behaviours.md",  # just placeholder examples
     ]
 
 
@@ -227,6 +226,7 @@ class TestPythonSnippets(BaseTestDocCode):
         "docs/demos/price_oracle_technical_details.md",  # price oracle has been extracted to a separate repo on #1441
         "docs/advanced_reference/developer_tooling/benchmarking.md",  # just placeholder examples
         "docs/configure_service/on-chain_deployment_checklist.md",  # just placeholder examples
+        "docs/advanced_reference/testing_behaviours.md",  # just placeholder examples
     ]
 
 
