5a1746c043d7fff7243ae7320fb3b603863c9f81
OjusWiZard <ojuswimail@gmail.com>
2026-03-03 02:57:29 +0530

    chore: bump `open-aea==0.21.12rc3`
    Signed-off-by: OjusWiZard <ojuswimail@gmail.com>

--- autonomy/constants.py
+++ autonomy/constants.py
@@ -65,5 +65,5 @@ DEFAULT_SERVICE_REGISTRY_CONTRACTS_IMAGE = (
 ACN_IMAGE_NAME = os.environ.get("ACN_IMAGE_NAME", "valory/open-acn-node")
 DEFAULT_DOCKER_IMAGE_AUTHOR = "valory"
 OAR_IMAGE = "{image_author}/oar-{agent}:{version}"
-ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeieo3yo2qsqsvw234qct4kkr7hqic2ectupndimxzqpmkfuopbrz2a"
+ABSTRACT_ROUND_ABCI_SKILL_WITH_HASH = "valory/abstract_round_abci:0.1.0:bafybeibtithky3lzsbxuygszvbn7kul6okfi7e7hc5rczy2ystfnst2lci"
 OLAS_DOCS_URL = "https://stack.olas.network"
